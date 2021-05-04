cdc4b3c469ccaa47f62ab7d08a30c84716baa3db
Joeylene Rivera <joeylenerivera@gmail.com>
2021-05-04 18:09:07 +0800

    feat: add param validation to group's connect method

--- powerbase-server/app/controllers/groups_controller.rb
+++ powerbase-server/app/controllers/groups_controller.rb
@@ -1,13 +1,23 @@
 class GroupsController < ApplicationController
+  schema(:connect) do
+    optional(:host).value(:string)
+    optional(:port).value(:integer)
+    optional(:username).value(:string)
+    optional(:password).value(:string)
+    optional(:database).value(:string)
+    optional(:connection_string).value(:string)
+  end
+
   # POST /bases/connect
   def connect
     db = Powerbase.connect({
       adapter:  "postgres",
-      host:     params[:host],
-      port:     params[:port],
-      user:     params[:username],
-      password: params[:password],
-      database: params[:database],
+      host:     safe_params[:host],
+      port:     safe_params[:port],
+      user:     safe_params[:username],
+      password: safe_params[:password],
+      database: safe_params[:database],
+      connection_string: safe_params[:connection_string],
     })
 
     render json: { connected: db.test_connection }
