60be4a0a0db3470bee9e38e6799852b925c6ea50
Joeylene Rivera <joeylenerivera@gmail.com>
2021-05-04 09:43:34 +0800

    tests: add tables controller test

--- powerbase-server/test/controllers/tables_controller_test.rb
+++ powerbase-server/test/controllers/tables_controller_test.rb
@@ -1,7 +1,22 @@
 require "test_helper"
 
 class TablesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  def connect_database
+    post groups_connect_url,
+      params: {
+        host: "localhost",
+        port: "5432",
+        username: "postgres",
+        password: "postgres",
+        database: "powerbase_test",
+      },
+      as: :json
+  end
+
+  test "should get list of tables of connected group" do
+    connect_database()
+
+    get groups_tables_url
+    assert_response :success
+  end
 end
