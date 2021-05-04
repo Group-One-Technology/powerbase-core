690cac4bcec0cb96b6cea4a58b2f3be46c1386af
Joeylene Rivera <joeylenerivera@gmail.com>
2021-05-04 09:43:27 +0800

    tests: add group controller test

--- powerbase-server/test/controllers/groups_controller_test.rb
+++ powerbase-server/test/controllers/groups_controller_test.rb
@@ -1,7 +1,17 @@
 require "test_helper"
 
 class GroupsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should connect to a group" do
+    post groups_connect_url,
+      params: {
+        host: "localhost",
+        port: "5432",
+        username: "postgres",
+        password: "postgres",
+        database: "powerbase_test",
+      },
+      as: :json
+
+    assert_response :success
+  end
 end
