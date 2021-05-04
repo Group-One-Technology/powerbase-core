c774b043e4b68113ce5510a44adee13eb3196134
Joeylene Rivera <joeylenerivera@gmail.com>
2021-05-05 06:43:23 +0800

    feat: add encryption key var for group connection string attribute

--- powerbase-server/app/models/group.rb
+++ powerbase-server/app/models/group.rb
@@ -3,6 +3,6 @@ class Group < ApplicationRecord
   validates :connection_string, presence: true
   enum database_type: { postgres: "postgres" }, _prefix: :database
 
-  # TODO: Add Figaro Gem for key env
-  attr_encrypted :connection_string, key: 'some_key', algorithm: 'aes-256-cbc', mode: :single_iv_and_salt, insecure_mode: true
+  attr_encrypted :connection_string, key: ENV["encyrption_key"],
+    algorithm: "aes-256-cbc", mode: :single_iv_and_salt, insecure_mode: true
 end
