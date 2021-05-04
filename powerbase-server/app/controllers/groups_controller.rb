a870442cb9797caf5627b06eb2ffec257c0d3105
Joeylene Rivera <joeylenerivera@gmail.com>
2021-05-04 09:00:11 +0800

    feat: add groups controller with connect method

---
+++ powerbase-server/app/controllers/groups_controller.rb
@@ -0,0 +1,15 @@
+class GroupsController < ApplicationController
+  # POST /bases/connect
+  def connect
+    db = Powerbase.connect({
+      adapter:  "postgres",
+      host:     params[:host],
+      port:     params[:port],
+      user:     params[:username],
+      password: params[:password],
+      database: params[:database],
+    })
+
+    render json: { connected: db.test_connection, tables: db.tables }
+  end
+end
