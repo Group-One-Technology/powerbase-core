41b689adf5b8c40ec5874c130c0deb473dbdb00d
Joeylene Rivera <joeylenerivera@gmail.com>
2021-05-06 00:36:07 +0800

    refactor: rename field type variable in test case

--- powerbase-server/test/models/powerbase_field_type_test.rb
+++ powerbase-server/test/models/powerbase_field_type_test.rb
@@ -2,12 +2,12 @@ require "test_helper"
 
 class PowerbaseFieldTypeTest < ActiveSupport::TestCase
   test "should save field type with valid data" do
-    field = powerbase_field_types(:one)
-    assert field.save, "Could not save the field with valid data"
+    field_type = powerbase_field_types(:one)
+    assert field_type.save, "Could not save the field type with valid data"
   end
 
-  test "should not save field without required fields" do
-    field = PowerbaseFieldType.new
-    assert_not field.save, "Saved the field without required fields"
+  test "should not save field type without required fields" do
+    field_type = PowerbaseFieldType.new
+    assert_not field_type.save, "Saved the type field without required fields"
   end
 end
