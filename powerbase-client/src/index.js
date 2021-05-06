19b2d79861aa43f9f0d6a1f54c12bf17de97034e
Joeylene Rivera <joeylenerivera@gmail.com>
2021-05-06 22:07:38 +0800

    chore: add classnames

--- powerbase-client/src/index.js
+++ powerbase-client/src/index.js
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
+import cn from 'classnames';
 import './index.css';
 
 const App = () => {
+  const [active, setActive] = useState(false);
+
   return (
     <div className="p-4 bg-gray-800 text-white h-screen w-full flex items-center justify-center">
       <div className="w-96 text-center">
-        <h1 className="text-3xl">Hello World</h1>
+        <h1 className={cn('text-3xl', { 'text-blue-400': active })}>
+          Hello World
+        </h1>
         <p>{process.env.NODE_ENV}</p>
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          onClick={() => setActive((state) => !state)}
+        >
+          Toggle Text Color
+        </button>
       </div>
     </div>
   );
