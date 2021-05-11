0d7b9220d9657dd6a3854aa6eee2b41e38b28610
Joeylene Rivera <joeylenerivera@gmail.com>
2021-05-12 02:19:22 +0800

    refactor: add react state to form

--- powerbase-client/src/pages/LoginPage.jsx
+++ powerbase-client/src/pages/LoginPage.jsx
@@ -1,6 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export function LoginPage() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const onEmailChange = (evt) => setEmail(evt.target.value);
+  const onPasswordChange = (evt) => setPassword(evt.target.value);
+
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+    console.log('submit');
+  };
+
   return (
     <div className="min-h-screen bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
       <div className="sm:mx-auto sm:w-full sm:max-w-md">
@@ -20,7 +31,7 @@ export function LoginPage() {
 
       <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
         <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
-          <form className="space-y-6" action="#" method="POST">
+          <form className="space-y-6" onSubmit={handleSubmit}>
             <div>
               <label htmlFor="email" className="block text-sm font-medium text-gray-700">
                 Email address
@@ -31,6 +42,8 @@ export function LoginPage() {
                   name="email"
                   type="email"
                   autoComplete="email"
+                  value={email}
+                  onChange={onEmailChange}
                   required
                   className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
                 />
@@ -47,6 +60,8 @@ export function LoginPage() {
                   name="password"
                   type="password"
                   autoComplete="current-password"
+                  value={password}
+                  onChange={onPasswordChange}
                   required
                   className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
                 />
