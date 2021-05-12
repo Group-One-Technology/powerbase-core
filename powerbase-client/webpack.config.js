c0d80c019f1cd0b7a7824b2bb1c8d2cfbe9ae928
Joeylene Rivera <joeylenerivera@gmail.com>
2021-05-13 06:52:24 +0800

    feat: add logo and assets alias

--- powerbase-client/webpack.config.js
+++ powerbase-client/webpack.config.js
@@ -13,6 +13,7 @@ module.exports = {
       '@pages': path.resolve(__dirname, 'src/pages'),
       '@components': path.resolve(__dirname, 'src/components'),
       '@lib': path.resolve(__dirname, 'src/lib'),
+      '@assets': path.resolve(__dirname, 'src/assets'),
     },
     extensions: ['.js', '.jsx'],
   },
@@ -36,7 +37,7 @@ module.exports = {
         ],
       },
       {
-        test: /\.(png|jpe?g|gif)$/i,
+        test: /\.(png|jpe?g|gif|svg)$/i,
         use: [
           {
             loader: 'file-loader',
