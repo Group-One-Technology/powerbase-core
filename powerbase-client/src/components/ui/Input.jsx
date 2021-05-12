ad2f9bb0d865a0c3116b6b5ad30fbc93e59e77e8
Joeylene Rivera <joeylenerivera@gmail.com>
2021-05-13 06:41:57 +0800

    feat: add error validation to input

--- powerbase-client/src/components/ui/Input.jsx
+++ powerbase-client/src/components/ui/Input.jsx
@@ -1,12 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import cn from 'classnames';
 
-export function Input(props) {
+export function Input({
+  id,
+  label,
+  name,
+  error,
+  onFocus,
+  onBlur,
+  onChange,
+  showError,
+  ...props
+}) {
+  const [focused, setFocused] = useState(false);
+  const [inputId] = useState(() => id || (label ? `formInput-${label.replace(/\s+/g, '-')}` : undefined));
+
+  const focus = (evt) => {
+    if (onBlur)  onFocus(evt);
+
+    setFocused(true);
+  };
+
+  const blur = (evt) => {
+    if (onBlur) onBlur(evt);
+
+    setFocused(false);
+  };
+
+  const change = onChange || ((evt) => {
+    if (setValue) setValue(evt.target.value);
+  });
+
+  const showErrorText = !!(showError || (!focused && error));
+
   return (
-    <input
-      className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-      {...props}
-    />
+    <>
+      <input
+        id={inputId}
+        name={name || label || props['aria-label']}
+        onFocus={focus}
+        onBlur={blur}
+        onChange={change}
+        className={cn('appearance-none block w-full px-3 py-2 border rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm', {
+          [showErrorText ? 'border-red-400': 'border-gray-300']: true
+        })}
+        {...props}
+      />
+      {(showErrorText && error) && (
+        <p className="text-xs text-red-400 my-2">
+          {error.message}
+        </p>
+      )}
+    </>
   );
 }
 
@@ -15,7 +61,11 @@ Input.propTypes = {
   type: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
   value: PropTypes.any.isRequired,
+  error: PropTypes.object,
+  showError: PropTypes.bool,
   onChange: PropTypes.func.isRequired,
+  onFocus: PropTypes.func,
+  onBlur: PropTypes.func,
   autoComplete: PropTypes.string,
   required: PropTypes.bool,
 }
