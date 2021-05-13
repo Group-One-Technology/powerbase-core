e868d6450fc6b7df9f4a51512bde30dcd9986672
Joeylene Rivera <joeylenerivera@gmail.com>
2021-05-13 09:08:16 +0800

    feat: add loading state to login

--- powerbase-client/src/pages/LoginPage.jsx
+++ powerbase-client/src/pages/LoginPage.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useHistory } from 'react-router';
 
 import logoImg from '@assets/img/logo.svg';
@@ -15,13 +15,18 @@ export function LoginPage() {
 
   const [email, setEmail, { error: emailError }] = useValidState('', EMAIL_VALIDATOR);
   const [password, setPassword, { error: passwordError }] = useValidState('', PASSWORD_VALIDATOR);
+  const [loading, setLoading] = useState(false);
 
   const onEmailChange = (evt) => setEmail(evt.target.value);
   const onPasswordChange = (evt) => setPassword(evt.target.value);
 
   const handleSubmit = (evt) => {
     evt.preventDefault();
+    setLoading(true);
+
     setAuthUser(userMock);
+
+    setLoading(false)
   };
 
   useEffect(() => {
@@ -43,7 +48,7 @@ export function LoginPage() {
 
       <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
         <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
-          <form className="space-y-6" onSubmit={handleSubmit}>
+          <form className="space-y-6" onSubmit={handleSubmit} aria-busy={loading}>
             <Input
               id="email"
               label="Email address"
@@ -91,7 +96,14 @@ export function LoginPage() {
               <button
                 type="submit"
                 className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                disabled={loading}
               >
+                {loading && (
+                  <svg className="animate-spin -ml-3 mr-3 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                    <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
+                    <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                  </svg>
+                )}
                 Sign in
               </button>
             </div>
