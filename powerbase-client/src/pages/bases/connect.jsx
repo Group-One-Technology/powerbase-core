6d6c84f2e31faaeb1568c4f415089cb558ab042d
Joeylene Rivera <joeylenerivera@gmail.com>
2021-05-17 09:56:09 +0800

    feat: add database type select to connect base

--- powerbase-client/src/pages/bases/connect.jsx
+++ powerbase-client/src/pages/bases/connect.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, Fragment } from 'react';
+import React, { useEffect, Fragment, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import { InboxIcon } from '@heroicons/react/outline';
 import { Menu, Transition } from '@headlessui/react';
@@ -6,17 +6,21 @@ import { ChevronDownIcon } from '@heroicons/react/solid';
 import cn from 'classnames';
 
 import { useAuthUser } from '@models/AuthUser';
+import { useValidState } from '@lib/hooks/useValidState';
+import { REQUIRED_VALIDATOR } from '@lib/validators/REQUIRED_VALIDATOR';
+import { DATABASE_TYPES } from '@lib/constants';
+
 import { Navbar } from '@components/layout/Navbar';
 import { Page } from '@components/layout/Page';
 import { PageHeader } from '@components/layout/PageHeader';
 import { InlineInput } from '@components/ui/InlineInput';
 import { PageContent } from '@components/layout/PageContent';
-import { useValidState } from '@lib/hooks/useValidState';
-import { REQUIRED_VALIDATOR } from '@lib/validators/REQUIRED_VALIDATOR';
+import { InlineSelect } from '@components/ui/InlineSelect';
 
 export function BasesConnectPage() {
   const history = useHistory();
   const [databaseName, setDatabaseName, databaseNameError] = useValidState('', REQUIRED_VALIDATOR);
+  const [databaseType, setDatabaseType] = useState(DATABASE_TYPES[0]);
 
   return (
     <Page authOnly>
@@ -33,6 +37,12 @@ export function BasesConnectPage() {
               value={databaseName}
               onChange={(evt) => setDatabaseName(evt.target.value)}
               error={databaseNameError.error}
+            />
+            <InlineSelect
+              label="Type"
+              value={databaseType}
+              setValue={setDatabaseType}
+              options={DATABASE_TYPES}
             />
           </div>
         </PageContent>
