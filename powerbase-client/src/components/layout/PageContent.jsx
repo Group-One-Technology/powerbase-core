dd58e9f756d7fd809ee3c138b0d4e51bda3ce2f8
Joeylene Rivera <joeylenerivera@gmail.com>
2021-05-17 09:10:08 +0800

    refactor: add classname to page content

--- powerbase-client/src/components/layout/PageContent.jsx
+++ powerbase-client/src/components/layout/PageContent.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import cn from 'classnames';
 import PropTypes from 'prop-types';
 
-export function PageContent({ children }) {
+export function PageContent({ children, className }) {
   return (
-    <main className="max-w-7xl mx-auto sm:px-6 lg:px-8">
+    <main className={cn('max-w-7xl mx-auto sm:px-6 lg:px-8', className)}>
       {children}
     </main>
   );
