629eeb791f8b0593d3590ead360bd12eff09c94c
Joeylene Rivera <joeylenerivera@gmail.com>
2021-05-17 20:26:03 +0800

    feat: add db platforms constant

--- powerbase-client/src/lib/constants.js
+++ powerbase-client/src/lib/constants.js
@@ -2,7 +2,12 @@ export const SITE_NAME = 'Powerbase';
 export const SITE_DESCRIPTION = 'Powerbase is the missing bridge to the worlds most trusted relational database.';
 
 export const DATABASE_TYPES = [
-  { name: 'Postgres' },
+  { name: 'PostgreSQL' },
   { name: 'MySQL', description: 'Coming Soon', disabled: true },
   { name: 'SQLite', description: 'Coming Soon', disabled: true },
+];
+
+export const DB_PLATFORMS = [
+  { name: 'Powerbase Cloud', description: 'A free cloud platform great for hobby and MVP apps.', price: '$0' },
+  { name: 'AWS', description: 'Coming Soon', disabled: true },
 ];
