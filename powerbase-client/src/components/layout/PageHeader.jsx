9b37385f6e68ab7e3bb18c81153078d2198db96e
Joeylene Rivera <joeylenerivera@gmail.com>
2021-05-17 09:10:17 +0800

    refactor: add classname to page header

--- powerbase-client/src/components/layout/PageHeader.jsx
+++ powerbase-client/src/components/layout/PageHeader.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import cn from 'classnames';
 
-export function PageHeader({ children }) {
+export function PageHeader({ children, className }) {
   return (
     <header className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-      <h1 className="text-3xl font-bold leading-tight text-gray-900 pb-4">
+      <h1 className={cn('text-3xl font-bold leading-tight text-gray-900 pb-4', className)}>
         {children}
       </h1>
     </header>
@@ -12,6 +13,7 @@ export function PageHeader({ children }) {
 };
 
 PageHeader.propTypes = {
+  className: PropTypes.string,
   children: PropTypes.oneOfType([
     PropTypes.string,
     PropTypes.element,
