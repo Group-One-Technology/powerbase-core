8f7a6c9d3e81610aa632a28e4a47a20360f9b3c9
Joeylene Rivera <joeylenerivera@gmail.com>
2021-05-19 08:01:26 +0800

    feat: add refresh controller

---
+++ powerbase-server/app/controllers/concerns/users/refresh_controller.rb
@@ -0,0 +1,23 @@
+class Users::RefreshController < ApplicationController
+  before_action :authorize_refresh_by_access_request!
+
+  def create
+    session = JWTSessions::Session.new(payload: claimless_payload, refresh_by_access_allowed: true)
+    tokens = session.refresh_by_access_payload do
+      raise JWTSessions::Errors::Unathorized, "Somethings not right here!"
+    end
+
+    response.set_cookie(
+      JWTSessions.access_cookie,
+      value: tokens[:access],
+      httponly: true,
+      secure: Rails.env.production?
+    )
+
+    render json: { csrf: tokens[:csrf] }
+  end
+
+  def access_payload
+    build_access_payload_based_on_refresh(payload)
+  end
+end
