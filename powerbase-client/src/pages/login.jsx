1ae5a9c969c4082bc1758a124bc7a46a0a0fdc32
Joeylene Rivera <joeylenerivera@gmail.com>
2021-05-20 20:02:06 +0800

    refactor: replace button in login page

--- powerbase-client/src/pages/login.jsx
+++ powerbase-client/src/pages/login.jsx
@@ -9,6 +9,7 @@ import { useAuthUser } from '@models/AuthUser';
 import { Page } from '@components/layout/Page';
 import { Input } from '@components/ui/Input';
 import { login } from '@lib/api/auth';
+import { Button } from '@components/ui/Button';
 
 export function LoginPage() {
   const history = useHistory();
@@ -104,21 +105,13 @@ export function LoginPage() {
               </div>
             </div>
 
-            <div>
-              <button
-                type="submit"
-                className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                disabled={loading}
-              >
-                {loading && (
-                  <svg className="animate-spin -ml-3 mr-3 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-                    <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
-                    <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
-                  </svg>
-                )}
-                Sign in
-              </button>
-            </div>
+            <Button
+              type="submit"
+              className="w-full flex justify-center"
+              loading={loading}
+            >
+              Sign in
+            </Button>
           </form>
 
           <div className="mt-6">
