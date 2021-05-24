7e9107564c0527dfb0b74fff942c6a91e2cd2b3f
Joeylene Rivera <joeylenerivera@gmail.com>
2021-05-25 04:50:18 +0800

    feat: add base page

--- powerbase-client/src/pages/bases/[id].jsx
+++ powerbase-client/src/pages/bases/[id].jsx
@@ -2,7 +2,8 @@ import React, { useEffect } from 'react';
 import useSWR from 'swr';
 import { useParams } from 'react-router-dom';
 
-import { getDatabase } from '@lib/api/databases';
+import { getDatabase, getDatabases } from '@lib/api/databases';
+import { useAuthUser } from '@models/AuthUser';
 import { Page } from '@components/layout/Page';
 import { PageHeader } from '@components/layout/PageHeader';
 import { Navbar } from '@components/layout/Navbar';
@@ -10,13 +11,15 @@ import { PageContent } from '@components/layout/PageContent';
 
 export function BasePage() {
   const { id } = useParams();
+  const { authUser } = useAuthUser();
+  const { data: bases } = useSWR(authUser ? '/databases' : null, getDatabases);
   const { data: base } = useSWR(id ? `/databases/${id}` : null, () => getDatabase({ id }));
 
   return (
-    <Page navbar={<Navbar base={base} />} className="!bg-white" authOnly>
+    <Page navbar={<Navbar base={base} bases={bases} />} className="!bg-white" authOnly>
       <div className="py-10 ">
         <PageContent className="bg-white">
-          sdfds
+          {base?.name}
         </PageContent>
       </div>
     </Page>
