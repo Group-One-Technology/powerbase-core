3f919892cf4e8eda7983fabd09d89b0ab3bcec6d
Joeylene Rivera <joeylenerivera@gmail.com>
2021-05-24 22:23:46 +0800

    feat: add bases to home

--- powerbase-client/src/pages/home.jsx
+++ powerbase-client/src/pages/home.jsx
@@ -1,106 +1,55 @@
 import React, { useEffect, Fragment } from 'react';
+import useSWR from 'swr';
 import { useHistory, Link } from 'react-router-dom';
-import { InboxIcon } from '@heroicons/react/outline';
-import { Menu, Transition } from '@headlessui/react';
-import { ChevronDownIcon } from '@heroicons/react/solid';
-import cn from 'classnames';
+import { PlusCircleIcon } from '@heroicons/react/outline';
 
 import { useAuthUser } from '@models/AuthUser';
+import { BaseItem } from '@components/bases/BaseItem';
+
 import { Navbar } from '@components/layout/Navbar';
 import { Page } from '@components/layout/Page';
 import { PageHeader } from '@components/layout/PageHeader';
 import { PageContent } from '@components/layout/PageContent';
+import { getDatabases } from '@lib/api/databases';
+import { EmptyBase } from '@components/bases/EmptyBase';
 
 export function HomePage() {
   const history = useHistory();
+  const authUser = useAuthUser();
+  const { data: bases} = useSWR(authUser ? '/databases' : null, getDatabases);
 
   return (
     <Page authOnly>
       <div className="py-10">
-        <PageHeader>
-          Bases
-        </PageHeader>
+        {bases?.length === 0 && (
+          <PageHeader>
+            Bases
+          </PageHeader>
+        )}
         <PageContent>
-          <div className="px-4 py-8 sm:px-0 border-4 border-dashed border-gray-200 rounded-lg h-96 flex items-center justify-center">
-            <div className="text-center">
-              <p className="text-lg text-gray-500 pb-4">
-                Looks lke you haven't added any databases.
-              </p>
-              <Menu as="div" className="relative inline-block text-left">
-                {({ open }) => (
-                  <>
-                    <div>
-                      <Menu.Button className="inline-flex justify-center w-full px-8 py-2 rounded-md border border-gray-300 shadow-sm bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500">
-                        Add Database
-                        <ChevronDownIcon className="-mr-1 ml-2 h-5 w-5" aria-hidden="true" />
-                      </Menu.Button>
-                    </div>
-
-                    <Transition
-                      show={open}
-                      as={Fragment}
-                      enter="transition ease-out duration-100"
-                      enterFrom="transform opacity-0 scale-95"
-                      enterTo="transform opacity-100 scale-100"
-                      leave="transition ease-in duration-75"
-                      leaveFrom="transform opacity-100 scale-100"
-                      leaveTo="transform opacity-0 scale-95"
-                    >
-                      <Menu.Items
-                        static
-                        className="origin-top-right absolute right-0 mt-2 w-full rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none"
-                      >
-                        <div className="py-1">
-                          <Menu.Item>
-                            {({ active }) => (
-                              <Link
-                                to="/bases/create"
-                                className={cn('block px-4 py-2 text-sm', (
-                                  active
-                                    ? 'bg-gray-100 text-gray-900'
-                                    : 'text-gray-700'
-                                ))}
-                              >
-                                Create New Database
-                              </Link>
-                            )}
-                          </Menu.Item>
-                          <Menu.Item>
-                            {({ active }) => (
-                              <Link
-                                to="/bases/connect"
-                                className={cn('block px-4 py-2 text-sm', (
-                                  active
-                                    ? 'bg-gray-100 text-gray-900'
-                                    : 'text-gray-700'
-                                ))}
-                              >
-                                Connect A Database You Own
-                              </Link>
-                            )}
-                          </Menu.Item>
-                          <Menu.Item>
-                            {({ active }) => (
-                              <Link
-                                to="/bases/connect-url"
-                                className={cn('block px-4 py-2 text-sm', (
-                                  active
-                                    ? 'bg-gray-100 text-gray-900'
-                                    : 'text-gray-700'
-                                ))}
-                              >
-                                Connect from a URL
-                              </Link>
-                            )}
-                          </Menu.Item>
-                        </div>
-                      </Menu.Items>
-                    </Transition>
-                  </>
-                )}
-              </Menu>
-            </div>
-          </div>
+          {!!bases?.length && (
+            <ul className="mt-16 flex justify-center items-center flex-wrap gap-6">
+              {bases.map((base) => (
+                <li
+                  key={base.id}
+                  className="w-48 flex flex-col text-center bg-white rounded-lg shadow divide-y divide-gray-200"
+                >
+                  <BaseItem key={base.id} base={base} />
+                </li>
+              ))}
+              <li className="w-48 flex flex-col text-center bg-gray-200 rounded-lg shadow divide-y divide-gray-200">
+                <Link to="/bases/create">
+                  <div className="flex-1 flex flex-col p-8 items-center justify-center">
+                    <PlusCircleIcon className="mt-3 h-12 w-12 text-gray-500" />
+                    <p className="mt-2 text-sm text-gray-500">
+                      Add New
+                    </p>
+                  </div>
+                </Link>
+              </li>
+            </ul>
+          )}
+          {bases?.length === 0 && <EmptyBase />}
         </PageContent>
       </div>
     </Page>
