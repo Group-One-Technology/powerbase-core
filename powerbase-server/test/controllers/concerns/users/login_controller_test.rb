eb7f9b26378506fb5783dc524ad9f125fd88cf23
Joeylene Rivera <joeylenerivera@gmail.com>
2021-05-25 00:13:35 +0800

    test: return login assert for tokens

--- powerbase-server/test/controllers/concerns/users/login_controller_test.rb
+++ powerbase-server/test/controllers/concerns/users/login_controller_test.rb
@@ -6,6 +6,8 @@ class LoginControllerTest < ApplcationControllerTest
 
   test "should be able to login" do
     login
+    assert cookies[:jwt_access], "JWT Access Cookie could not be saved."
+    assert JSON.parse(response.body)["csrf"], "CSRF token is missing in the response."
     assert_response :success
 
     logout
@@ -14,6 +16,8 @@ class LoginControllerTest < ApplcationControllerTest
   test "should not be able to login with invalid credentials" do
     login("notexistinguser@example.com", "12345678")
     assert_response :not_found
+
+    logout
   end
 
   test "should be able to logout" do
