20c736434b0bd18a9baec42d2e624b71ad85d618
Joeylene Rivera <joeylenerivera@gmail.com>
2021-05-26 04:52:07 +0800

    refactor: display tables count on migrated bases only

--- powerbase-client/src/components/bases/BaseItem.jsx
+++ powerbase-client/src/components/bases/BaseItem.jsx
@@ -14,17 +14,19 @@ export function BaseItem({ base }) {
     >
       <div className="p-8">
         <h2 className="mt-3 text-gray-900 text-xl font-bold uppercase break-words" style={{ hyphens: 'auto' }}>{base.name}</h2>
-        <dl className="mt-1 flex-grow flex flex-col justify-between">
-          <dt className="sr-only">No. of Tables</dt>
-          <dd className="text-gray-500 text-sm">{base.totalTables} tables</dd>
-        </dl>
         {base.isMigrated
           ? (
-            <p className="mt-1 ml-2 font-medium text-sm text-indigo-600 hover:text-indigo-500 inline-flex items-center self-center">
-              View
-              {' '}
-              <ChevronRightIcon className="h-4 w-4" />
-            </p>
+            <>
+              <dl className="mt-1 flex-grow flex flex-col justify-between">
+                <dt className="sr-only">No. of Tables</dt>
+                <dd className="text-gray-500 text-sm">{base.totalTables} tables</dd>
+              </dl>
+              <p className="mt-1 ml-2 font-medium text-sm text-indigo-600 hover:text-indigo-500 inline-flex items-center self-center">
+                View
+                {' '}
+                <ChevronRightIcon className="h-4 w-4" />
+              </p>
+            </>
           )
           : <Badge color="yellow" className="mt-2">Migrating</Badge>}
       </div>
