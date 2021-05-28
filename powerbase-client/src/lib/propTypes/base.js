fc51f2ed9170f19b320a3ebfd306d8231e4e4cec
Joeylene Rivera <joeylenerivera@gmail.com>
2021-05-29 02:20:11 +0800

    refactor: relocate colors array in base prop types

--- powerbase-client/src/lib/propTypes/base.js
+++ powerbase-client/src/lib/propTypes/base.js
@@ -1,12 +1,14 @@
 import PropTypes from 'prop-types';
 
+export const COLORS = ['gray', 'red', 'yellow', 'green', 'blue', 'indigo', 'purple', 'pink'];
+
 export const IBase = PropTypes.shape({
   id: PropTypes.number.isRequired,
   userId: PropTypes.number.isRequired,
   adapter: PropTypes.oneOf(['postgresql', 'mysql2']).isRequired,
   name: PropTypes.string.isRequired,
   description: PropTypes.string,
-  color: PropTypes.oneOf(['gray', 'red', 'yellow', 'green', 'blue', 'indigo', 'purple', 'pink']).isRequired,
+  color: PropTypes.oneOf(COLORS).isRequired,
   isMigrated: PropTypes.bool.isRequired,
   createdAt: PropTypes.string.isRequired,
   updatedAt: PropTypes.string.isRequired,
