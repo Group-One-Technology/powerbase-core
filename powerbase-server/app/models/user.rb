d5676ce3a0df71972c33592cf3a4381ed2027c4c
Joeylene Rivera <joeylenerivera@gmail.com>
2021-05-31 16:22:32 +0800

    feat: add elasticsearch to powerbase database

--- powerbase-server/app/models/user.rb
+++ powerbase-server/app/models/user.rb
@@ -4,4 +4,6 @@ class User < ApplicationRecord
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, presence: true
+
+  has_many :powerbase_databases
 end
