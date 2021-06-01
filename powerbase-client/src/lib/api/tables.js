094b6ee2e8dff8850caf4d4b2e47e734a751f85f
Joeylene Rivera <joeylenerivera@gmail.com>
2021-06-01 17:36:42 +0800

    feat: add get table api

--- powerbase-client/src/lib/api/tables.js
+++ powerbase-client/src/lib/api/tables.js
@@ -1,5 +1,15 @@
 import { securedApi } from './index';
 
+export async function getTable({ id }) {
+  const response = await securedApi.get(`/tables/${id}`);
+
+  if (response.statusText === 'OK') {
+    return response.data;
+  }
+
+  return undefined;
+}
+
 export async function getTables({ databaseId }) {
   const response = await securedApi.get(`/databases/${databaseId}/tables`);
 
--- powerbase-server/app/controllers/powerbase_tables_controller.rb
+++ powerbase-server/app/controllers/powerbase_tables_controller.rb
@@ -6,9 +6,19 @@ class PowerbaseTablesController < ApplicationController
     required(:database_id).value(:string)
   end
 
+  schema(:show) do
+    required(:id).value(:string)
+  end
+
   # GET /databases/:database_id/tables
   def index
     render json: { migrated: @database.is_migrated, tables: @database.powerbase_tables }
+  end
+
+  # GET /tables/:id
+  def show
+    @table = PowerbaseTable.includes(:powerbase_database).find(safe_params[:id])
+    render json: format_json(@table)
   end
 
   private
@@ -18,5 +28,29 @@ class PowerbaseTablesController < ApplicationController
       if !@database
         raise StandardError.new("Must establish a database connection first to execute this action.")
       end
+    end
+
+    def format_json(table)
+      database = table.powerbase_database
+
+      {
+        id: table.id,
+        name: table.name,
+        description: table.description,
+        created_at: table.created_at,
+        updated_at: table.updated_at,
+        database_id: table.powerbase_database_id,
+        database: {
+          id: database.id,
+          user_id: database.user_id,
+          adapter: database.adapter,
+          name: database.name,
+          description: database.description,
+          color: database.color,
+          is_migrated: database.is_migrated,
+          created_at: database.created_at,
+          updated_at: database.updated_at,
+        }
+      }
     end
 end
