e541e2f8658df9c394f565a0a790e29438b48140
Joeylene Rivera <joeylenerivera@gmail.com>
2021-06-02 15:05:43 +0800

    feat: add horizontal scroll to table tabs

--- powerbase-client/src/components/tables/TableTabs.jsx
+++ powerbase-client/src/components/tables/TableTabs.jsx
@@ -1,9 +1,11 @@
-import React from 'react';
-import { PlusIcon } from '@heroicons/react/solid';
+import React, { useEffect, useRef } from 'react';
+import { PlusIcon, ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/solid';
 import { Link, useHistory } from 'react-router-dom';
 import cn from 'classnames';
 import PropTypes from 'prop-types';
 import { BG_COLORS } from '@lib/constants';
+
+const SCROLL_OFFSET = 200;
 
 export function TableTabs({
   color,
@@ -12,13 +14,54 @@ export function TableTabs({
   tables,
 }) {
   const history = useHistory();
+  const tabsContainerEl = useRef();
+  const activeTabEl = useRef();
+
+  useEffect(() => {
+    activeTabEl.current?.scrollIntoView({ behavior: 'smooth' });
+  }, []);
+
+  const handleScroll = (position) => {
+    if (tabsContainerEl.current) {
+      const scrollWidth = tabsContainerEl.current.scrollWidth
+      const scrollOffsetWidth = tabsContainerEl.current.offsetWidth - SCROLL_OFFSET;
+      const scrollPosition = tabsContainerEl.current.scrollLeft;
+
+      let scrollTo = position === 'right'
+        ? scrollPosition + scrollOffsetWidth
+        : scrollPosition - scrollOffsetWidth
+
+      if (scrollTo <= 0) {
+        scrollTo = 0;
+      } else if (scrollTo >= scrollWidth - scrollOffsetWidth) {
+        scrollTo = scrollWidth;
+      }
+
+      tabsContainerEl.current.scroll({ left: scrollTo, behavior: 'smooth' });
+
+      const leftArrowEl = document.getElementById('tableTabsLeftArrow');
+      const rightArrowEl = document.getElementById('tableTabsRightArrow');
+
+      if (position === 'left' && scrollTo <= 0) {
+        leftArrowEl.classList.add('invisible');
+      } else {
+        leftArrowEl.classList.remove('invisible');
+      }
+
+      if (position === 'right' && scrollTo >= scrollWidth) {
+        rightArrowEl.classList.add('invisible');
+      } else {
+        rightArrowEl.classList.remove('invisible');
+      }
+    }
+  };
 
   const addTable = () => {
     alert('add new table clicked');
   };
 
   return (
-    <div className={cn('px-4 sm:px-6 lg:px-8 w-full overflow-auto', BG_COLORS[color])}>
+    <div className={cn('px-4 sm:px-6 lg:px-8 w-full', BG_COLORS[color])}>
       <div className="pb-2 sm:hidden">
         <label htmlFor="tabs" className="sr-only">
           Select a tab
@@ -44,7 +87,16 @@ export function TableTabs({
         </select>
       </div>
       <div className="hidden sm:flex">
-        <nav className="inline-flex space-x-1" aria-label="Tabs">
+        <button
+          id="tableTabsLeftArrow"
+          type="button"
+          className="relative inline-flex items-center m-2 p-0.5 rounded-full font-medium text-gray-200 bg-gray-900 bg-opacity-20 hover:bg-gray-900 hover:bg-opacity-25"
+          onClick={() => handleScroll('left')}
+        >
+          <span className="sr-only">Previous</span>
+          <ChevronLeftIcon className="h-4 w-4" aria-hidden="true" />
+        </button>
+        <nav ref={tabsContainerEl} className="inline-flex space-x-1 overflow-auto scrollbar-none" aria-label="Tabs">
           {tables == null && (
             <>
               <span className="sr-only">Loading the database' tables.</span>
@@ -65,6 +117,7 @@ export function TableTabs({
             return (
               <Link
                 key={table.id}
+                ref={isCurrentTable ? activeTabEl : undefined}
                 to={`/base/${databaseId}/table/${table.id}`}
                 className={cn(
                   'px-3 py-2 font-medium text-sm rounded-tl-md rounded-tr-md',
@@ -89,6 +142,15 @@ export function TableTabs({
             </div>
           )}
         </nav>
+        <button
+          id="tableTabsRightArrow"
+          type="button"
+          className="relative inline-flex items-center m-2 p-0.5 rounded-full font-medium text-gray-200 bg-gray-900 bg-opacity-20 hover:bg-gray-900 hover:bg-opacity-25"
+          onClick={() => handleScroll('right')}
+        >
+          <span className="sr-only">Previous</span>
+          <ChevronRightIcon className="h-4 w-4" aria-hidden="true" />
+        </button>
       </div>
     </div>
   );
