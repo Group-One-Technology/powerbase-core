d719246319455d16cac3ee9b94b1dc12e6c8b059
Joeylene Rivera <joeylenerivera@gmail.com>
2021-06-04 18:42:08 +0800

    refactor: relocate cell renderer

--- powerbase-client/src/components/tables/BaseTables/index.jsx
+++ powerbase-client/src/components/tables/BaseTables/index.jsx
@@ -1,70 +1,23 @@
 import { useWindowSize } from '@lib/hooks/useWindowSize';
 import React, { useState } from 'react';
 import { ArrowKeyStepper, Grid, AutoSizer } from 'react-virtualized';
-import cn from 'classnames';
 import PropTypes from 'prop-types';
 
-import { FieldTypeIcon } from '@components/ui/FieldTypeIcon';
+import { CellRenderer } from './CellRenderer';
 import 'react-virtualized/styles.css';
 
 export function BaseTable({ fields }) {
   const windowSize = useWindowSize();
   const columnCount = fields?.length;
-  const rowCount = 1;
+  const rowCount = 2;
   const records = fields
-    ? [['', ...fields.map((item) => item.name)]]
+    ? [['', ...fields.map((item) => item.name)], [1, ...fields.map((item) => item.name)]]
     : undefined;
 
   const [currentCell, setCurrentCell] = useState({
     row: 0,
     column: 0,
   });
-
-  const cellRenderer = ({
-    columnIndex,
-    key,
-    rowIndex,
-    style,
-    value,
-  }) => {
-    const isHeader = rowIndex === 0;
-    const isRowNo = columnIndex === 0;
-
-    return (
-      <div
-        role="button"
-        id={`row-${rowIndex}_col-${columnIndex}`}
-        key={key}
-        className={cn(
-          'single-line text-sm truncate focus:bg-gray-100 border-b border-r border-gray-200 flex items-center py-1 px-2',
-          isHeader && 'bg-gray-100',
-          isRowNo && 'flex justify-center text-xs text-gray-500',
-        )}
-        onClick={() => setCurrentCell({ row: rowIndex, column: columnIndex })}
-        style={style}
-        tabIndex={0}
-        onKeyDown={(evt) => {
-          const el = evt.target;
-
-          if (evt.code === 'Enter' && !isRowNo) {
-            el.contentEditable = el.contentEditable !== 'true';
-          } else if (el.contentEditable !== 'true') {
-            setCurrentCell({ row: rowIndex, column: columnIndex });
-          }
-        }}
-        onDoubleClick={(evt) => {
-          if (!isRowNo) evt.target.contentEditable = true;
-        }}
-        onBlur={(evt) => {
-          if (!isRowNo) evt.target.contentEditable = false;
-        }}
-        suppressContentEditableWarning
-      >
-        {(isHeader && columnIndex !== 0) && <FieldTypeIcon className="mr-1" />}
-        {value}
-      </div>
-    );
-  };
 
   const handleScrollToChange = ({ scrollToRow, scrollToColumn }) => {
     const editableElement = document.querySelector('[contenteditable=true]');
@@ -102,11 +55,12 @@ export function BaseTable({ fields }) {
                 onSectionRendered={onSectionRendered}
                 scrollToColumn={scrollToColumn}
                 scrollToRow={scrollToRow}
-                cellRenderer={({ rowIndex, columnIndex, ...props }) => cellRenderer({
+                cellRenderer={({ rowIndex, columnIndex, ...props }) => CellRenderer({
                   scrollToColumn,
                   scrollToRow,
                   rowIndex,
                   columnIndex,
+                  setCurrentCell,
                   value: records[rowIndex][columnIndex],
                   ...props,
                 })}
