d37d8427eeb857343d2568397bd11b9b21c5b188
Joeylene Rivera <joeylenerivera@gmail.com>
2021-06-14 16:08:37 +0800

    feat: add elasticsearch to powerbase table for turbo dbs

--- powerbase-server/app/models/powerbase_table.rb
+++ powerbase-server/app/models/powerbase_table.rb
@@ -1,4 +1,9 @@
+require 'elasticsearch/model'
+
 class PowerbaseTable < ApplicationRecord
+  include Elasticsearch::Model
+  scope :turbo, -> { joins(:powerbase_database).where(powerbase_database: { is_turbo: true }) }
+
   validates :name, presence: true
 
   belongs_to :powerbase_database
@@ -6,4 +11,8 @@ class PowerbaseTable < ApplicationRecord
   has_many :table_foreign_keys
   has_many :table_foreign_keys, foreign_key: :referenced_table_id
   has_many :table_views
+
+  after_commit on: [:create] do
+    logger.debug ["Saving document... ", __elasticsearch__.index_document ].join if self.powerbase_database.is_turbo
+  end
 end
