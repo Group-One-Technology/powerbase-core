8f20cb0f8f3d9c84c4de4cfaef38182bd5a0518e
Joeylene Rivera <joeylenerivera@gmail.com>
2021-06-18 22:09:59 +0800

    feat: integrate sequel-elasticsearch

--- powerbase-server/app/lib/powerbase/model.rb
+++ powerbase-server/app/lib/powerbase/model.rb
@@ -1,3 +1,5 @@
+require "sequel/plugins/elasticsearch"
+
 module Powerbase
   class Model
     # Initialize the Powerbase::Model
@@ -7,12 +9,14 @@ module Powerbase
       @esclient = Elasticsearch::Client.new
 
       @es_table = @esclient.get(index: "powerbase_tables", id: table_id, ignore: 404)
-      @is_turbo = !!@es_table['found']
+      @is_turbo = !!@es_table["found"]
 
       if @is_turbo
-        @es_table = @es_table['_source']
-        @table_name = @es_table['name']
-        @es_database = @esclient.get(index: "powerbase_databases", id: @es_table['powerbase_database_id'])['_source']
+        @es_table = @es_table["_source"]
+        @table_name = @es_table["name"]
+        @es_database = @esclient.get(index: "powerbase_databases", id: @es_table['powerbase_database_id'])["_source"]
+
+        connect_remote_db(table_id)
       else
         connect_remote_db(table_id)
         @table_name = @powerbase_table.name
@@ -58,7 +62,7 @@ module Powerbase
           }
         )
 
-        result['hits']['hits'].map {|result| result['_source']}
+        result["hits"]["hits"].map {|result| result["_source"]}
       else
         puts "Retrieving table #{@table_id}'s records from remote database..."
         @remote_table.all
@@ -70,59 +74,14 @@ module Powerbase
       index = "table_records_#{@table_id}"
 
       if @is_turbo
-        filter = { query: { bool: {} } }
-
-        predicates = ["lt", "gt", "lte", "gte", "eq", "not_eq", "matches"]
-
-        field_mappings = @esclient.perform_request("GET", "#{index}/_mappings").body[index]
-        field_mappings = field_mappings['mappings']['properties']
-
-        filter_params.each do |key, val|
-          dup_key = key.dup
-          field_key = dup_key.gsub(/\_(#{predicates.join("|")})$/, "")&.to_sym
-          predicate = dup_key.sub!(/^#{field_key}_/, "")
-
-          if ["lt", "gt", "lte", "gte"].include? predicate
-            filter[:query][:bool][:filter] = [] if !filter[:query][:bool].key?(:filter)
-
-            range_filter = {}
-            range_filter[field_key] = {}
-            range_filter[field_key][predicate.to_sym] = val
-
-            filter[:query][:bool][:filter].push({ range: range_filter })
-          elsif predicate == "matches"
-            filter[:query][:bool][:must] = [] if !filter[:query][:bool].key?(:must)
-
-            wildcard_filter = {}
-            wildcard_filter[field_key] = val
-
-            filter[:query][:bool][:must].push({ match_phrase: wildcard_filter })
-          elsif predicate == nil or predicate == "eq"
-            filter[:query][:bool][:filter] = [] if !filter[:query][:bool].key?(:filter)
-
-            current_field_mapping = field_mappings.find {|k, v| k.to_s == field_key.to_s}
-
-            filter_field_key = if current_field_mapping && current_field_mapping[1]['type'] == "date"
-                field_key.to_sym
-              elsif val.is_a?(String)
-                "#{field_key}.keyword".to_sym
-              else
-                field_key.to_sym
-              end
-
-            term_filter = {}
-            term_filter[filter_field_key] = val
-
-            filter[:query][:bool][:filter].push({ term: term_filter })
-          end
+        model = Class.new(Sequel::Model(@remote_table)) do
+          plugin :elasticsearch, index: index
         end
 
-        result = @esclient.search(
-          index: "table_records_#{@table_id}",
-          body: filter
-        )
+        # TODO: Update Elasticsearch filtering
+        results = model.es('title:"Beta Launch"')
 
-        result['hits']['hits'].map {|result| result['_source']}
+        return results
       else
         Philtre.new(filter_params).apply(@remote_table)
       end
