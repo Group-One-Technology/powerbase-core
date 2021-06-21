f766e56d873357e3b637fef282bff14f331ac31c
Joeylene Rivera <joeylenerivera@gmail.com>
2021-06-22 02:28:39 +0800

    feat: add Id prop type

--- powerbase-client/src/lib/propTypes/view_field.js
+++ powerbase-client/src/lib/propTypes/view_field.js
@@ -1,7 +1,8 @@
 import PropTypes from 'prop-types';
+import { IId } from './common';
 
 export const IViewField = PropTypes.shape({
-  id: PropTypes.number.isRequired,
+  id: IId.isRequired,
   name: PropTypes.string.isRequired,
   description: PropTypes.string.isRequired,
   default_value: PropTypes.any.isRequired,
@@ -10,9 +11,9 @@ export const IViewField = PropTypes.shape({
   order: PropTypes.number.isRequired,
   is_frozen: PropTypes.bool.isRequired,
   is_hidden: PropTypes.bool.isRequired,
-  view_id: PropTypes.number.isRequired,
-  table_id: PropTypes.number.isRequired,
-  field_type_id: PropTypes.number.isRequired,
+  view_id: IId.isRequired,
+  table_id: IId.isRequired,
+  field_type_id: IId.isRequired,
   created_at: PropTypes.string.isRequired,
   updated_at: PropTypes.string.isRequired,
 });
