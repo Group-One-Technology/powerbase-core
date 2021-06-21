ad2e3853b05fc068ca0bab6e994a208e60d61276
Joeylene Rivera <joeylenerivera@gmail.com>
2021-06-21 23:27:17 +0800

    fix: remove order from powerbase field

--- powerbase-server/app/models/powerbase_field.rb
+++ powerbase-server/app/models/powerbase_field.rb
@@ -1,7 +1,6 @@
 class PowerbaseField < ApplicationRecord
   validates :name, presence: true
   validates :db_type, presence: true
-  validates :order, presence: true
 
   belongs_to :powerbase_table
   belongs_to :powerbase_field_type
