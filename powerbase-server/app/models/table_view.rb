0228e1cf6d4f3e78fec438159246082f32524651
Joeylene Rivera <joeylenerivera@gmail.com>
2021-06-26 06:33:55 +0800

    feat: add model relation between table and view

--- powerbase-server/app/models/table_view.rb
+++ powerbase-server/app/models/table_view.rb
@@ -2,7 +2,9 @@ class TableView < ApplicationRecord
   validates :name, presence: true
   validates :view_type, presence: true
   enum view_type: { grid: "grid" }, _prefix: true
+  serialize :filters, Hash
 
   belongs_to :powerbase_table
   has_many :view_field_options
+  has_one :powerbase_table, foreign_key: :default_view_id
 end
