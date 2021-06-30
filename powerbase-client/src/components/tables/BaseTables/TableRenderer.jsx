667e3217d721ead3fa768ec50777499ed583dcd1
Joeylene Rivera <joeylenerivera@gmail.com>
2021-07-01 01:39:03 +0800

    refactor: remove arrowstepper

--- powerbase-client/src/components/tables/BaseTables/TableRenderer.jsx
+++ powerbase-client/src/components/tables/BaseTables/TableRenderer.jsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { ArrowKeyStepper, Grid, AutoSizer } from 'react-virtualized';
+import React from 'react';
+import { Grid, AutoSizer } from 'react-virtualized';
 import PropTypes from 'prop-types';
 
 import { IViewField } from '@lib/propTypes/view_field';
@@ -13,59 +13,26 @@ export function TableRenderer({ fields, records, height }) {
     ...records,
   ];
 
-  const [currentCell, setCurrentCell] = useState({
-    row: 0,
-    column: 0,
-  });
-
-  const handleScrollToChange = ({ scrollToRow, scrollToColumn }) => {
-    const editableElement = document.querySelector('[contenteditable=true]');
-
-    if (!editableElement) {
-      setCurrentCell({ row: scrollToRow, column: scrollToColumn });
-      const focusedCell = document.getElementById(`row-${scrollToRow}_col-${scrollToColumn}`);
-      focusedCell.focus();
-    }
-  };
-
   return (
     <div className="w-full overflow-hidden z-0">
-      <ArrowKeyStepper
-        mode="cells"
-        columnCount={columnCount}
-        rowCount={rowCount}
-        onScrollToChange={handleScrollToChange}
-        scrollToColumn={currentCell.column}
-        scrollToRow={currentCell.row}
-        isControlled
-      >
-        {({ onSectionRendered, scrollToColumn, scrollToRow }) => (
-          <AutoSizer disableHeight>
-            {({ width }) => (
-              <Grid
-                onSectionRendered={onSectionRendered}
-                scrollToColumn={scrollToColumn}
-                scrollToRow={scrollToRow}
-                cellRenderer={({ rowIndex, columnIndex, ...props }) => CellRenderer({
-                  scrollToColumn,
-                  scrollToRow,
-                  rowIndex,
-                  columnIndex,
-                  setCurrentCell,
-                  value: tableValues[rowIndex][columnIndex],
-                  ...props,
-                })}
-                columnWidth={({ index }) => (index === 0 ? 50 : fields[index - 1].width)}
-                columnCount={columnCount}
-                rowHeight={30}
-                rowCount={rowCount}
-                height={height}
-                width={width}
-              />
-            )}
-          </AutoSizer>
+      <AutoSizer disableHeight>
+        {({ width }) => (
+          <Grid
+            cellRenderer={({ rowIndex, columnIndex, ...props }) => CellRenderer({
+              rowIndex,
+              columnIndex,
+              value: tableValues[rowIndex][columnIndex],
+              ...props,
+            })}
+            columnWidth={({ index }) => (index === 0 ? 50 : fields[index - 1].width)}
+            columnCount={columnCount}
+            rowHeight={30}
+            rowCount={rowCount}
+            height={height}
+            width={width}
+          />
         )}
-      </ArrowKeyStepper>
+      </AutoSizer>
     </div>
   );
 }
