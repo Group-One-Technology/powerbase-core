653945e6bb5ab8932226845a01ee41113d9c699a
Joeylene Rivera <joeylenerivera@gmail.com>
2021-07-10 04:00:05 +0800

    refactor: remove uneeded fragment

--- powerbase-client/src/components/views/TableViewsNav/index.jsx
+++ powerbase-client/src/components/views/TableViewsNav/index.jsx
@@ -1,6 +1,4 @@
-/* eslint-disable no-shadow */
-/* eslint-disable jsx-a11y/anchor-is-valid */
-import React, { Fragment } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { SearchIcon, SwitchVerticalIcon, ShareIcon } from '@heroicons/react/outline';
 
@@ -23,51 +21,49 @@ export function TableViewsNav({
   const { data: totalRecords } = useTableRecordsCount();
 
   return (
-    <>
-      <div className="w-full px-4 sm:px-6 lg:px-8 border-solid border-b-2 border-gray-200 text-gray-700">
-        <div className="relative flex  py-1.5 gap-x-2">
-          <div className="flex-1 flex items-center gap-x-2">
-            <TableViewsSelect
-              baseId={baseId}
-              tableId={tableId}
-              currentView={currentView}
-              views={views}
-            />
-            {!!(records && totalRecords) && (
-              <p className="text-xs hidden lg:inline">
-                {records.length} loaded out of {totalRecords}
-              </p>
-            )}
-          </div>
-          <div className="flex-1 flex items-center justify-center gap-x-2">
-            <TableViewsFilter view={currentView} fields={fields} />
-            <button
-              type="button"
-              className="inline-flex items-center px-1.5 py-1 border border-transparent text-xs font-medium rounded text-gray-700 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
-            >
-              <span className="sr-only">Sort fields</span>
-              <SwitchVerticalIcon className="block h-4 w-4" />
-            </button>
-            <button
-              type="button"
-              className="inline-flex items-center px-1.5 py-1 border border-transparent text-xs font-medium rounded text-gray-700 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
-            >
-              <span className="sr-only">Share this view</span>
-              <ShareIcon className="block h-4 w-4" />
-            </button>
-          </div>
-          <div className="flex-1 flex items-center justify-end">
-            <button
-              type="button"
-              className="inline-flex items-center px-1.5 py-1 border border-transparent text-xs font-medium rounded text-gray-700 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
-            >
-              <span className="sr-only">Search</span>
-              <SearchIcon className="block h-4 w-4" />
-            </button>
-          </div>
+    <div className="w-full px-4 sm:px-6 lg:px-8 border-solid border-b-2 border-gray-200 text-gray-700">
+      <div className="relative flex  py-1.5 gap-x-2">
+        <div className="flex-1 flex items-center gap-x-2">
+          <TableViewsSelect
+            baseId={baseId}
+            tableId={tableId}
+            currentView={currentView}
+            views={views}
+          />
+          {!!(records && totalRecords) && (
+            <p className="text-xs hidden lg:inline">
+              {records.length} loaded out of {totalRecords}
+            </p>
+          )}
+        </div>
+        <div className="flex-1 flex items-center justify-center gap-x-2">
+          <TableViewsFilter view={currentView} fields={fields} />
+          <button
+            type="button"
+            className="inline-flex items-center px-1.5 py-1 border border-transparent text-xs font-medium rounded text-gray-700 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
+          >
+            <span className="sr-only">Sort fields</span>
+            <SwitchVerticalIcon className="block h-4 w-4" />
+          </button>
+          <button
+            type="button"
+            className="inline-flex items-center px-1.5 py-1 border border-transparent text-xs font-medium rounded text-gray-700 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
+          >
+            <span className="sr-only">Share this view</span>
+            <ShareIcon className="block h-4 w-4" />
+          </button>
+        </div>
+        <div className="flex-1 flex items-center justify-end">
+          <button
+            type="button"
+            className="inline-flex items-center px-1.5 py-1 border border-transparent text-xs font-medium rounded text-gray-700 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
+          >
+            <span className="sr-only">Search</span>
+            <SearchIcon className="block h-4 w-4" />
+          </button>
         </div>
       </div>
-    </>
+    </div>
   );
 }
 
