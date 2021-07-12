15bf99e314a82e3e87234b1aaf5541ec6c207ab4
Joeylene Rivera <joeylenerivera@gmail.com>
2021-07-13 02:15:00 +0800

    refactor: rename output from build to public

--- powerbase-client/tailwind.config.js
+++ powerbase-client/tailwind.config.js
@@ -3,7 +3,7 @@ const defaultTheme = require('tailwindcss/defaultTheme');
 module.exports = {
   mode: 'jit',
   purge: [
-    './build/*.html',
+    './public/*.html',
     './src/**/*.{js,jsx}',
   ],
   darkMode: 'class',
