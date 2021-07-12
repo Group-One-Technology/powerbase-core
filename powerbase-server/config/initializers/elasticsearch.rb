f569b436a0c76b90a7a57509c7922510f480e10e
Joeylene Rivera <joeylenerivera@gmail.com>
2021-07-13 02:10:00 +0800

    refactor: update elasticsearch log condition

--- powerbase-server/config/initializers/elasticsearch.rb
+++ powerbase-server/config/initializers/elasticsearch.rb
@@ -6,5 +6,5 @@ elasticsearch_url = if Rails.env.production?
     ENV["elasticsearch_url_development"]
   end
 
-ElasticsearchClient = Elasticsearch::Client.new url: elasticsearch_url, log: true
-Elasticsearch::Model.client = Elasticsearch::Client.new url: elasticsearch_url, log: true
+ElasticsearchClient = Elasticsearch::Client.new url: elasticsearch_url, log: !Rails.env.production?
+Elasticsearch::Model.client = Elasticsearch::Client.new url: elasticsearch_url, !Rails.env.production?
