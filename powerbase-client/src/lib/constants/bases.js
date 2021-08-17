b827bbe223a95b17fa919dbfe38e56b278e92c22
Joeylene Rivera <joeylenerivera@gmail.com>
2021-08-18 07:39:40 +0800

    feat: add base sources constant

---
+++ powerbase-client/src/lib/constants/bases.js
@@ -0,0 +1,7 @@
+export const BASE_SOURCES = [
+  { name: 'Fresh Base', description: 'Build out your table and data from scratch', footnote: 'Options: Postgres, MySQL' },
+  { pretext: 'Connect Existing', name: 'Postgres', footnote: 'Connect to your existing postgres database' },
+  { pretext: 'Connect Existing', name: 'MySQL', footnote: 'Connect to your existing mysql database' },
+  { name: 'Hubspot', description: 'Connect your Hubspot account', footnote: 'Campaigns, Companies, Contacts, Contact Lists, Deals, Deal Pipelines, Email Events, +3 More' },
+  { name: 'Shopify', description: 'Connect your Shopify account', footnote: 'Customers, Orders, Products, Abandoned Checkouts' },
+];
