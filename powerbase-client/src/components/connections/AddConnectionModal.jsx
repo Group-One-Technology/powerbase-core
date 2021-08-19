0a73c68d5899c4bb89bd11846293b2d6e73b59a1
Joeylene Rivera <joeylenerivera@gmail.com>
2021-08-20 01:54:25 +0800

    refactor: update save connection submit text

--- powerbase-client/src/components/connections/AddConnectionModal.jsx
+++ powerbase-client/src/components/connections/AddConnectionModal.jsx
@@ -66,7 +66,7 @@ export function AddConnectionModal({
               type="submit"
               className="ml-5 bg-sky-700 border border-transparent rounded-md shadow-sm py-2 px-4 inline-flex justify-center text-sm font-medium text-white hover:bg-sky-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-sky-500"
             >
-              Add Connection
+              Save Connection
             </button>
           </div>
         </form>
