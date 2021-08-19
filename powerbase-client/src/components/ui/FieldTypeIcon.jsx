ad0bb6a8143b0e22452077e0f28cc1e67c54bd10
Joeylene Rivera <joeylenerivera@gmail.com>
2021-08-20 07:44:24 +0800

    feat: add icons to field type icon ui

--- powerbase-client/src/components/ui/FieldTypeIcon.jsx
+++ powerbase-client/src/components/ui/FieldTypeIcon.jsx
@@ -1,13 +1,59 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import cn from 'classnames';
+import {
+  TemplateIcon,
+  CheckCircleIcon,
+  HashtagIcon,
+  LightningBoltIcon,
+  MailIcon,
+  MenuAlt1Icon,
+  CalendarIcon,
+  ChevronDownIcon,
+} from '@heroicons/react/solid';
 
-export function FieldTypeIcon({ className }) {
-  return (
-    <span className={cn('text-sm font-medium text-gray-600', className)}>A</span>
-  );
+// TODO: Replace with API
+const FIELD_TYPES = [
+  'Single Line Text',
+  'Long Text',
+  'Checkbox',
+  'Number',
+  'Single Select',
+  'Multiple Selct',
+  'Date',
+  'Email',
+  'Plugin',
+  'Others',
+];
+
+export function FieldTypeIcon({ typeId, className }) {
+  const generatedClassName = cn('text-sm text-gray-600 h-4 w-4', className);
+
+  switch (FIELD_TYPES[typeId]) {
+    case 'Long Text':
+      return <MenuAlt1Icon className={generatedClassName} />;
+    case 'Checkbox':
+      return <CheckCircleIcon className={generatedClassName} />;
+    case 'Number ':
+      return <HashtagIcon className={generatedClassName} />;
+    case 'Single Select':
+      return <ChevronDownIcon className={generatedClassName} />;
+    case 'Multiple Select':
+      return <ChevronDownIcon className={generatedClassName} />;
+    case 'Date':
+      return <CalendarIcon className={generatedClassName} />;
+    case 'Email':
+      return <MailIcon className={generatedClassName} />;
+    case 'Plugin':
+      return <LightningBoltIcon className={generatedClassName} />;
+    case 'Others':
+      return <TemplateIcon className={generatedClassName} />;
+    default:
+      return <span className={cn('text-sm font-medium text-gray-600 flex items-center', className)}>A</span>;
+  }
 }
 
 FieldTypeIcon.propTypes = {
+  typeId: PropTypes.number,
   className: PropTypes.string,
 };
