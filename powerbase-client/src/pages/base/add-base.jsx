85ef5eac49be2c57e811d6379fcc30ad419390a3
Joeylene Rivera <joeylenerivera@gmail.com>
2021-08-20 01:39:28 +0800

    fix: update key prop of base sources

--- powerbase-client/src/pages/base/add-base.jsx
+++ powerbase-client/src/pages/base/add-base.jsx
@@ -17,7 +17,7 @@ export function AddBasePage() {
           <ul className="mt-4 flex flex-col justify-center flex-wrap gap-5 sm:flex-row sm:mx-48">
             {BASE_SOURCES.map((source) => (
               <li
-                key={source.nme}
+                key={source.name}
                 className="sm:w-56 sm:h-60 text-center bg-white rounded-lg shadow divide-y divide-gray-200"
               >
                 <BaseSourceItem source={source} />
