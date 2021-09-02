fb2af08740996b91c86c51ff957388c8cdae4ee2
Joeylene Rivera <joeylenerivera@gmail.com>
2021-09-02 09:11:42 +0800

    feat: populate base connections with api

--- powerbase-client/src/components/bases/settings/BaseConnectionsSettings.jsx
+++ powerbase-client/src/components/bases/settings/BaseConnectionsSettings.jsx
@@ -5,43 +5,15 @@ import { ArrowRightIcon } from '@heroicons/react/outline';
 import { ITable } from '@lib/propTypes/table';
 import { IBase } from '@lib/propTypes/base';
 import { AddConnectionModal } from '@components/connections/AddConnectionModal';
-import { Loader } from '@components/ui/Loader';
 import { EditConnectionModal } from '@components/connections/EditConnectionModal';
+import { Loader } from '@components/ui/Loader';
 
-const MOCK_CONNECTIONS = (base, tables) => [
-  {
-    id: 1,
-    baseName: base.name,
-    tableName: tables[0].name,
-    columnName: 'email',
-    joinBaseName: 'Hubspot',
-    joinTableName: 'messages',
-    joinColumnName: 'email',
-  },
-  {
-    id: 2,
-    baseName: base.name,
-    tableName: tables[1].name,
-    columnName: 'email',
-    joinBaseName: 'Hubspot',
-    joinTableName: 'messages',
-    joinColumnName: 'email',
-  },
-  {
-    id: 3,
-    baseName: base.name,
-    tableName: tables[2].name,
-    columnName: 'path_id',
-    joinBaseName: base.name,
-    joinTableName: tables[3].name,
-    joinColumnName: 'path_id',
-  },
-];
-
-export function BaseConnectionsSettings({ base, bases, tables }) {
-  const connections = tables?.length
-    ? MOCK_CONNECTIONS(base, tables)
-    : [];
+export function BaseConnectionsSettings({
+  base,
+  bases,
+  tables,
+  connections,
+}) {
   const [openAddModal, setAddModalOpen] = useState(false);
   const [openEditModal, setEditModalOpen] = useState(false);
   const [selectedConnection, setSelectedConnection] = useState();
@@ -55,43 +27,24 @@ export function BaseConnectionsSettings({ base, bases, tables }) {
   return (
     <div className="py-6 px-4 sm:p-6 lg:pb-8">
       <h2 className="text-xl leading-6 font-medium text-gray-900">Connections</h2>
-      {bases == null && <Loader className="h-[50vh]" />}
+      {(bases == null || typeof connections === 'undefined') && <Loader className="h-[50vh]" />}
       {!!bases?.length && (
         <>
-          <ul className="mt-4">
-            {connections.map((connection) => (
-              <li key={connection.id} className="block py-2 hover:bg-gray-50">
-                <div className="grid grid-cols-12 gap-2 items-center w-full">
-                  <div className="col-span-5">
-                    <div className="max-w-lg flex text-sm">
-                      <span className="flex-none inline-flex items-center px-3 rounded-l-md border border-r-0 border-gray-300 bg-gray-50 text-gray-500">
-                        {connection.baseName}
-                      </span>
-                      <span className="inline-flex items-center px-3 text-gray-900 border border-r-0 border-gray-300">
-                        {connection.tableName}
-                      </span>
-                      <span className="inline-flex items-center px-3 rounded-r-md text-gray-900 border border-gray-300">
-                        {connection.columnName}
-                      </span>
-                    </div>
-                  </div>
-                  <div className="col-span-1 flex items-center justify-center">
-                    <ArrowRightIcon className="h-4 w-4 text-gray-500" />
-                  </div>
-                  <div className="col-span-5">
-                    <div className="max-w-lg flex text-sm">
-                      <span className="flex-none inline-flex items-center px-3 rounded-l-md border border-r-0 border-gray-300 bg-gray-50 text-gray-500">
-                        {connection.joinBaseName}
-                      </span>
-                      <span className="inline-flex items-center px-3 text-gray-900 border border-r-0 border-gray-300">
-                        {connection.joinTableName}
-                      </span>
-                      <span className="inline-flex items-center px-3 rounded-r-md text-gray-900 border border-gray-300">
-                        {connection.joinColumnName}
-                      </span>
-                    </div>
-                  </div>
-                  <div className="col-span-1 flex justify-end">
+          <ul className="mt-4 overflow-auto">
+            {connections?.map((connection) => {
+              const baseName = bases.find((item) => item.id === connection.databaseId)?.name;
+              const table = tables.find((item) => item.id === connection.tableId);
+              const tableName = table?.name || table?.alias || null;
+              const columnName = connection.columns.join(', ');
+
+              const joinBaseName = bases.find((item) => item.id === connection.referencedDatabaseId)?.name;
+              const joinTable = tables.find((item) => item.id === connection.referencedTableId);
+              const joinTableName = joinTable?.name || joinTable?.alias || null;
+              const joinColumnName = connection.referencedColumns.join(', ');
+
+              return (
+                <li key={connection.id} className="block py-2 hover:bg-gray-50">
+                  <div className="flex gap-3 items-center">
                     <button
                       type="button"
                       className="inline-flex items-center shadow-sm px-2.5 py-0.5 border border-gray-300 text-sm leading-5 font-medium rounded-full text-gray-700 bg-white hover:bg-gray-50"
@@ -99,10 +52,35 @@ export function BaseConnectionsSettings({ base, bases, tables }) {
                     >
                       Edit
                     </button>
+                    <div className="max-w-lg flex text-sm">
+                      <span className="flex-none inline-flex items-center px-3 rounded-l-md border border-r-0 border-gray-300 bg-gray-50 text-gray-500 whitespace-nowrap">
+                        {baseName}
+                      </span>
+                      <span className="inline-flex items-center px-3 text-gray-900 border border-r-0 border-gray-300 whitespace-nowrap">
+                        {tableName}
+                      </span>
+                      <span className="inline-flex items-center px-3 rounded-r-md text-gray-900 border border-gray-300 whitespace-nowrap">
+                        {columnName}
+                      </span>
+                    </div>
+                    <div className="flex items-center justify-center">
+                      <ArrowRightIcon className="h-4 w-4 text-gray-500" />
+                    </div>
+                    <div className="max-w-lg flex text-sm">
+                      <span className="flex-none inline-flex items-center px-3 rounded-l-md border border-r-0 border-gray-300 bg-gray-50 text-gray-500 whitespace-nowrap">
+                        {joinBaseName}
+                      </span>
+                      <span className="inline-flex items-center px-3 text-gray-900 border border-r-0 border-gray-300 whitespace-nowrap">
+                        {joinTableName}
+                      </span>
+                      <span className="inline-flex items-center px-3 rounded-r-md text-gray-900 border border-gray-300 whitespace-nowrap">
+                        {joinColumnName}
+                      </span>
+                    </div>
                   </div>
-                </div>
-              </li>
-            ))}
+                </li>
+              );
+            })}
           </ul>
           <EditConnectionModal
             open={openEditModal}
@@ -136,4 +114,5 @@ BaseConnectionsSettings.propTypes = {
   base: IBase.isRequired,
   bases: PropTypes.arrayOf(IBase),
   tables: PropTypes.arrayOf(ITable),
+  connections: PropTypes.arrayOf(PropTypes.object),
 };
