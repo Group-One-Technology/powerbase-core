9ebce74a6573074ed6000c6445c719a01fbe60a8
Joeylene Rivera <joeylenerivera@gmail.com>
2021-09-07 16:20:51 +0800

    fix: add column label to record title and update boolean to checkbox

--- powerbase-client/src/components/record/LinkedRecordItem.jsx
+++ powerbase-client/src/components/record/LinkedRecordItem.jsx
@@ -5,14 +5,25 @@ export function LinkedRecordItem({ record }) {
   return (
     <div className="p-2 border border-gray-300 rounded-lg">
       <p className="mb-2 text-sm font-medium text-gray-900 truncate">
-        {record[Object.keys(record)[0]]}
+        {Object.keys(record)[0].toUpperCase()}: {record[Object.keys(record)[0]]}
       </p>
       <div className="flex items-center gap-2">
         {Object.entries(record)
           .map(([key, value], recordIndex) => (recordIndex !== 0 && recordIndex <= 3) && (
             <div key={key} className="flex-1 overflow-hidden">
               <p className="text-xs text-gray-500 truncate">{key.toUpperCase()}</p>
-              <p className="text-xs text-gray-800 truncate h-[16px]">{value}</p>
+              <p className="text-xs text-gray-800 truncate h-[24px]">
+                {typeof value === 'boolean'
+                  ? (
+                    <input
+                      type="checkbox"
+                      className="mt-1 h-4 w-4 text-indigo-600 border-gray-300 rounded"
+                      checked={value}
+                      readOnly
+                    />
+                  )
+                  : value}
+              </p>
             </div>
           ))}
       </div>
