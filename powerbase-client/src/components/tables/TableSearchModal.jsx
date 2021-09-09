129d25adf0a71cfcd580485d902bd6334126bbcd
Francis Bulus <notchera@gmail.com>
2021-09-09 11:33:04 +0100

    implementing feedback for no table found in table search

--- powerbase-client/src/components/tables/TableSearchModal.jsx
+++ powerbase-client/src/components/tables/TableSearchModal.jsx
@@ -1,13 +1,14 @@
-/* eslint-disable  */
+/* eslint-disable no-unused-vars */
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
 /* eslint-disable react/prop-types */
 import React, {
   Fragment, useRef, useState, useEffect,
 } from 'react';
 import { Dialog, Transition } from '@headlessui/react';
-import { SearchIcon } from '@heroicons/react/outline';
+import { SearchIcon, ExclamationIcon } from '@heroicons/react/outline';
 
 export default function TableSearchModal({
-  open, setOpen, bgColor, tables, handleTableChange
+  open, setOpen, bgColor, tables, handleTableChange,
 }) {
   const [searchTerm, setSearchTerm] = useState('');
   const [searchResults, setSearchResults] = useState([]);
@@ -21,13 +22,17 @@ export default function TableSearchModal({
   }, [searchTerm]);
 
   const handleSearchResultClick = (table) => {
-      setOpen(false)
-      setSearchResults(tables)
-      return handleTableChange(table)
-  }
+    setOpen(false);
+    setSearchResults(tables);
+    return handleTableChange(table);
+  };
 
   const focusRef = useRef(null);
-  const color = bgColor.split('-')[1]
+  const searchInputRef = useRef(null);
+
+  useEffect(() => {
+    searchInputRef.current?.focus();
+  }, []);
 
   return (
     <Transition.Root show={open} as={Fragment}>
@@ -55,29 +60,37 @@ export default function TableSearchModal({
           >
             <div className="inline-block align-bottom bg-white rounded-md px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full sm:p-6">
               <div>
-                <div className="mx-auto flex items-center justify-center h-12" ref={focusRef}>
+                <div className="mx-auto flex items-center justify-center h-12">
                   <div className="mt-1 relative rounded-md shadow-sm w-full">
                     <input
                       type="text"
                       name="account-number"
                       id="account-number"
-                      className={`outline-none focus:outline-none block w-full pr-10 sm:text-sm border-gray-300 rounded-md`}
+                      className="outline-none focus:outline-none block w-full pr-10 sm:text-sm border-gray-300 rounded-md"
                       placeholder="Search for a table"
                       onChange={handleChange}
-                      autofocus={true}
-                      onBlur={({ target }) => target.focus()}
+                      ref={searchInputRef}
+                    //   onBlur={({ target }) => target.focus()}
                     />
                     <div className="absolute inset-y-0 right-0 pr-3 flex items-center pointer-events-none">
-                      <SearchIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
+                      <SearchIcon className="h-4 w-4 text-gray-400" aria-hidden="true" />
                     </div>
                   </div>
                 </div>
                 <div className="mt-2 text-center sm:mt-5">
+                  {searchResults && (
                   <div className="mt-1">
                     <ul>
-                      {searchResults && searchResults.map((item) => <li className={`p-2 text-sm cursor-pointer hover:bg-gray-200`} key={item?.id} onClick={() => handleSearchResultClick({ table: item })}>{item.alias || item.name}</li>)}
+                      {/* eslint-disable-next-line jsx-a11y/click-events-have-key-events */}
+                      {searchResults?.map((item) => <li className="p-2 text-sm cursor-pointer hover:bg-gray-200" key={item?.id} onClick={() => handleSearchResultClick({ table: item })}>{item.alias || item.name}</li>)}
                     </ul>
                   </div>
+                  )}{!searchResults.length && (
+                  <div className="flex flex-col justify-center content-center">
+                    <ExclamationIcon className="h-9 w-9 text-gray-400" />
+                    <div> There are no matching tables.</div>
+                  </div>
+                  )}
                 </div>
               </div>
             </div>
