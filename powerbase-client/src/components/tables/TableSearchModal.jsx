9521300c2910a8d02518c1d23b9d7daf42b4cacc
Francis Bulus <notchera@gmail.com>
2021-09-09 10:20:37 +0100

    _

--- powerbase-client/src/components/tables/TableSearchModal.jsx
+++ powerbase-client/src/components/tables/TableSearchModal.jsx
@@ -64,6 +64,8 @@ export default function TableSearchModal({
                       className={`outline-none focus:outline-none block w-full pr-10 sm:text-sm border-gray-300 rounded-md`}
                       placeholder="Search for a table"
                       onChange={handleChange}
+                      autofocus={true}
+                      onBlur={({ target }) => target.focus()}
                     />
                     <div className="absolute inset-y-0 right-0 pr-3 flex items-center pointer-events-none">
                       <SearchIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
