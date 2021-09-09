ee28b6bd883cdde071b6fa23eae18d98d8f4474f
Francis Bulus <notchera@gmail.com>
2021-09-09 07:11:05 +0100

    making minor tweaks to implementation

--- powerbase-client/src/components/tables/TableSearchModal.jsx
+++ powerbase-client/src/components/tables/TableSearchModal.jsx
@@ -28,7 +28,6 @@ export default function TableSearchModal({
 
   const focusRef = useRef(null);
   const color = bgColor.split('-')[1]
-  console.log(color)
 
   return (
     <Transition.Root show={open} as={Fragment}>
@@ -45,11 +44,6 @@ export default function TableSearchModal({
           >
             <Dialog.Overlay className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
           </Transition.Child>
-
-          {/* This element is to trick the browser into centering the modal contents. */}
-          {/* <span className="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">
-            &#8203;
-          </span> */}
           <Transition.Child
             as={Fragment}
             enter="ease-out duration-300"
@@ -76,8 +70,8 @@ export default function TableSearchModal({
                     </div>
                   </div>
                 </div>
-                <div className="mt-3 text-center sm:mt-5">
-                  <div className="mt-2">
+                <div className="mt-2 text-center sm:mt-5">
+                  <div className="mt-1">
                     <ul>
                       {searchResults && searchResults.map((item) => <li className={`p-2 text-sm cursor-pointer hover:bg-gray-200`} key={item?.id} onClick={() => handleSearchResultClick({ table: item })}>{item.alias || item.name}</li>)}
                     </ul>
