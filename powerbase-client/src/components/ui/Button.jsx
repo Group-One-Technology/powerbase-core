df88fca8514ccc1df9ce9bc23c9866a433172b53
Joeylene Rivera <joeylenerivera@gmail.com>
2021-09-10 14:57:32 +0800

    refactor: add forward ref to button ui

--- powerbase-client/src/components/ui/Button.jsx
+++ powerbase-client/src/components/ui/Button.jsx
@@ -2,24 +2,22 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Spinner } from './Spinner';
 
-export function Button({
+export const Button = React.forwardRef(({
   children,
   loading,
   disabled,
   ...props
-}) {
-  return (
-    <button disabled={disabled || loading} {...props}>
-      {children}
-      {loading && (
-        <>
-          <span className="sr-only">Loading.</span>
-          <Spinner className="h-4 w-4 ml-1 text-current" />
-        </>
-      )}
-    </button>
-  );
-}
+}, ref) => (
+  <button ref={ref} disabled={disabled || loading} {...props}>
+    {children}
+    {loading && (
+      <>
+        <span className="sr-only">Loading.</span>
+        <Spinner className="h-4 w-4 ml-1 text-current" />
+      </>
+    )}
+  </button>
+));
 
 Button.propTypes = {
   children: PropTypes.any.isRequired,
