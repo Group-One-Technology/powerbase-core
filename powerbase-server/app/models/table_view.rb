aba8b8970584fb49d596ba48cdff013f52653c2c
Joeylene Rivera <joeylenerivera@gmail.com>
2021-09-20 14:09:06 +0800

    refactor: update table views filter from hash to json

--- powerbase-server/app/models/table_view.rb
+++ powerbase-server/app/models/table_view.rb
@@ -2,7 +2,7 @@ class TableView < ApplicationRecord
   validates :name, presence: true
   validates :view_type, presence: true
   enum view_type: { grid: "grid" }, _prefix: true
-  serialize :filters, Hash
+  serialize :filters, JSON
 
   belongs_to :powerbase_table
   has_many :view_field_options
