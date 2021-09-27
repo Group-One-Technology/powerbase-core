a8ed982ce2e4bd1fbfae84cab8357787de48a225
Joeylene Rivera <joeylenerivera@gmail.com>
2021-09-27 17:58:24 +0800

    feat: add edit view

---
+++ powerbase-client/src/components/views/EditView.jsx
@@ -0,0 +1,181 @@
+import React, { Fragment, useEffect, useState } from 'react';
+import cn from 'classnames';
+import PropTypes from 'prop-types';
+import { Dialog, Transition, RadioGroup } from '@headlessui/react';
+import { TrashIcon } from '@heroicons/react/outline';
+
+import { useCurrentView } from '@models/views/CurrentTableView';
+import { updateTableView } from '@lib/api/views';
+import { VIEW_TYPES } from '@lib/constants/view';
+import { Badge } from '@components/ui/Badge';
+import { Button } from '@components/ui/Button';
+import { ErrorAlert } from '@components/ui/ErrorAlert';
+
+export function EditView({ view, open, setOpen }) {
+  const { viewsResponse } = useCurrentView();
+  const [name, setName] = useState(view.name);
+  const [viewType, setViewType] = useState(VIEW_TYPES.find((item) => item.value === view.viewType));
+
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState();
+
+  useEffect(() => {
+    setName(view.name);
+    setViewType(VIEW_TYPES.find((item) => item.value === view.viewType));
+  }, [view.id]);
+
+  const handleNameChange = (evt) => {
+    setName(evt.target.value);
+  };
+
+  const handleViewTypeChange = (value) => {
+    setViewType(value);
+  };
+
+  const handleSubmit = async (evt) => {
+    evt.preventDefault();
+    setLoading(true);
+    setError(undefined);
+
+    try {
+      await updateTableView({
+        id: view.id,
+        name,
+        viewType: viewType.value,
+      });
+
+      await viewsResponse.mutate();
+
+      setOpen(false);
+    } catch (err) {
+      setError(err.response.data.errors || err.response.data.exception);
+    }
+
+    setLoading(false);
+  };
+
+  return (
+    <Transition.Root show={open} as={Fragment}>
+      <Dialog
+        as="div"
+        static
+        className="fixed z-10 inset-0 overflow-y-auto"
+        open={open}
+        onClose={() => setOpen(false)}
+      >
+        <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <Dialog.Overlay className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+          </Transition.Child>
+
+          {/* This element is to trick the browser into centering the modal contents. */}
+          <span className="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">
+            &#8203;
+          </span>
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+            enterTo="opacity-100 translate-y-0 sm:scale-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+            leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+          >
+            <div className="inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full sm:p-6">
+              <form onSubmit={handleSubmit}>
+                {error && <ErrorAlert errors={error} />}
+                <input
+                  type="text"
+                  aria-label="View name"
+                  name="second_operand"
+                  value={name}
+                  onChange={handleNameChange}
+                  className="appearance-none block w-full h-8 px-2 py-1 text-sm border rounded-md shadow-sm placeholder-gray-400 border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+                  placeholder="View name"
+                  required
+                />
+                <RadioGroup value={viewType} onChange={handleViewTypeChange} className="mt-2">
+                  <RadioGroup.Label className="sr-only">
+                    View Type
+                  </RadioGroup.Label>
+                  <div className="flex flex-col space-y-2">
+                    {VIEW_TYPES.map((option) => (
+                      <RadioGroup.Option
+                        key={option.name}
+                        value={option}
+                        className={({ active }) => cn(
+                          'relative block rounded-lg bg-white border border-gray-300 shadow-sm p-4 cursor-pointer hover:border-gray-400 sm:flex sm:justify-between focus:outline-none',
+                          active && 'ring-1 ring-offset-2 ring-indigo-500',
+                          option.disabled && 'cursor-not-allowed',
+                        )}
+                        disabled={option.disabled}
+                      >
+                        {({ checked }) => (
+                          <>
+                            <div className="w-full text-sm">
+                              <RadioGroup.Label as="p" className="flex items-center font-medium text-gray-900">
+                                {option.name}
+                                {option.disabled && <Badge color="gray" className="ml-auto">Coming Soon</Badge>}
+                              </RadioGroup.Label>
+                              {option.description && (
+                                <RadioGroup.Description as="div" className="mt-0.5 text-xs text-gray-500">
+                                  <p className="sm:inline">
+                                    {option.description}
+                                  </p>
+                                </RadioGroup.Description>
+                              )}
+                            </div>
+                            <div aria-hidden="true" className={cn('absolute -inset-px rounded-lg border-2 pointer-events-none', checked ? 'border-indigo-500' : 'border-transparent')} />
+                          </>
+                        )}
+                      </RadioGroup.Option>
+                    ))}
+                  </div>
+                </RadioGroup>
+                <div className="mt-4 flex gap-2">
+                  <Button
+                    type="button"
+                    className="inline-flex items-center justify-center border border-transparent font-medium px-4 py-2 text-sm rounded-md shadow-sm text-red-400 bg-white hover:bg-red-300 hover:text-red-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-400"
+                    disabled={loading}
+                  >
+                    <TrashIcon className="h-5 w-5" aria-hidden="true" />
+                    <span className="sr-only">Delete</span>
+                  </Button>
+                  <Button
+                    type="button"
+                    className="ml-auto inline-flex items-center justify-center border border-transparent font-medium px-4 py-2 text-sm rounded-md shadow-sm text-gray-700 bg-white hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-200"
+                    disabled={loading}
+                    onClick={() => setOpen(false)}
+                  >
+                    Cancel
+                  </Button>
+                  <Button
+                    type="submit"
+                    className="inline-flex items-center justify-center border border-transparent font-medium px-4 py-2 text-sm rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                    loading={loading}
+                  >
+                    Update View
+                  </Button>
+                </div>
+              </form>
+            </div>
+          </Transition.Child>
+        </div>
+      </Dialog>
+    </Transition.Root>
+  );
+}
+
+EditView.propTypes = {
+  view: PropTypes.object.isRequired,
+  open: PropTypes.bool.isRequired,
+  setOpen: PropTypes.func.isRequired,
+};
--- powerbase-client/src/components/views/ViewMenu.jsx
+++ powerbase-client/src/components/views/ViewMenu.jsx
@@ -2,19 +2,28 @@ import React, { Fragment, useState } from 'react';
 import cn from 'classnames';
 import PropTypes from 'prop-types';
 import { Popover, Transition } from '@headlessui/react';
-import { PlusIcon, ViewGridIcon } from '@heroicons/react/outline';
+import { DotsHorizontalIcon, PlusIcon, ViewGridIcon } from '@heroicons/react/outline';
 
 import { useCurrentView } from '@models/views/CurrentTableView';
 import { IView } from '@lib/propTypes/view';
 import { IId } from '@lib/propTypes/common';
 import { AddView } from './AddView';
+import { EditView } from './EditView';
 
 export function ViewMenu({ tableId, currentView, views }) {
   const { handleViewChange } = useCurrentView();
   const [addViewModalOpen, setAddViewModalOpen] = useState(false);
+  const [viewOptionModal, setViewOptionModal] = useState({
+    open: false,
+    view: undefined,
+  });
 
   const handleAddView = () => {
     setAddViewModalOpen(true);
+  };
+
+  const handleViewOptions = (view) => {
+    setViewOptionModal({ open: true, view });
   };
 
   return (
@@ -33,22 +42,35 @@ export function ViewMenu({ tableId, currentView, views }) {
           leaveFrom="opacity-100 translate-y-0"
           leaveTo="opacity-0 translate-y-1"
         >
-          <Popover.Panel className="z-10 origin-top-right absolute right-0 mt-2 w-full rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
+          <Popover.Panel className="z-10 origin-top-right absolute left-0 mt-2 w-auto rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
             <div className="overflow-hidden text-sm text-gray-900 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5">
               <ul>
                 {views.map((view) => (
-                  <li key={view.id} className="whitespace-nowrap">
+                  <li
+                    key={view.id}
+                    className={cn(
+                      'whitespace-nowrap flex items-center w-auto hover:bg-gray-100 hover:text-gray-900 text-xs',
+                      view.id === currentView.id ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                    )}
+                  >
                     <button
                       type="button"
                       onClick={() => handleViewChange(view)}
-                      className={cn(
-                        'w-full flex items-center p-2 text-xs hover:bg-gray-100 hover:text-gray-900',
-                        view.id === currentView.id ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                      )}
+                      className="w-full flex items-center p-2"
                     >
                       <ViewGridIcon className="inline h-4 w-4 mr-1" />
                       {view.name}
                     </button>
+                    <div className="p-0.5">
+                      <button
+                        type="button"
+                        className="inline-flex items-center p-1.5 rounded-md text-gray-900 hover:bg-gray-200"
+                        onClick={() => handleViewOptions(view)}
+                      >
+                        <DotsHorizontalIcon className="h-4 w-4" aria-hidden="true" />
+                        <span className="sr-only">View Options</span>
+                      </button>
+                    </div>
                   </li>
                 ))}
                 <li>
@@ -67,6 +89,13 @@ export function ViewMenu({ tableId, currentView, views }) {
         </Transition>
       </Popover>
       <AddView tableId={tableId} open={addViewModalOpen} setOpen={setAddViewModalOpen} />
+      {viewOptionModal.view && (
+        <EditView
+          open={viewOptionModal.open}
+          setOpen={(value) => setViewOptionModal((curVal) => ({ ...curVal, open: value }))}
+          view={viewOptionModal.view}
+        />
+      )}
     </>
   );
 }
