c182adc934b43945e813f788cbab8dc14778e5d7
Joeylene Rivera <joeylenerivera@gmail.com>
2021-10-05 22:23:19 +0800

    refactor: replace react-movable with dnd-kit
    realized that react-movable doesn't support sorting horizontal elements.
    Moreover, it seems that dnd-kit uses hooks and can be used for kanban views

---
+++ powerbase-client/src/components/ui/SortableItem.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { useSortable } from '@dnd-kit/sortable';
+import { CSS } from '@dnd-kit/utilities';
+
+export function SortableItem({
+  as = 'div',
+  id,
+  children,
+  ...props
+}) {
+  const {
+    attributes,
+    listeners,
+    setNodeRef,
+    transform,
+    transition,
+  } = useSortable({ id });
+
+  const style = {
+    transform: CSS.Transform.toString(transform),
+    transition,
+  };
+
+  return React.createElement(
+    as,
+    {
+      ref: setNodeRef,
+      style,
+      ...attributes,
+      ...listeners,
+      ...props,
+    },
+    children,
+  );
+}
+
+SortableItem.propTypes = {
+  as: PropTypes.string,
+  id: PropTypes.any.isRequired,
+  children: PropTypes.any.isRequired,
+};
