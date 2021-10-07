71fbd767a90695273f4ac1ade8f9e3dbd4d52122
Joeylene Rivera <joeylenerivera@gmail.com>
2021-10-07 15:48:18 +0800

    refactor: add prop types to table header

--- powerbase-client/src/components/tables/VirtualTable/TableHeader.jsx
+++ powerbase-client/src/components/tables/VirtualTable/TableHeader.jsx
@@ -1,24 +1,49 @@
-/* eslint-disable react/prop-types */
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Grid } from 'react-virtualized';
+import { SCROLLBAR_WIDTH, ROW_NO_CELL_WIDTH, DEFAULT_CELL_WIDTH } from '@lib/constants';
 import { useFieldTypes } from '@models/FieldTypes';
 import { useViewFields } from '@models/ViewFields';
 import { FieldTypeIcon } from '@components/ui/FieldTypeIcon';
 
 function cellRenderer({
-  rowIndex, columnIndex, field, fieldTypes, style,
+  rowIndex,
+  columnIndex,
+  field,
+  fieldTypes,
+  style,
 }) {
-  if (columnIndex === 0) {
-    return <div key={`row-${rowIndex}-column-${columnIndex}`} className="bg-gray-100 text-sm focus:bg-gray-100 border-gray-200 border-r border-b py-1 px-2" style={style} />;
+  const key = `row-${rowIndex}-column-${columnIndex}`;
+
+  if (columnIndex === 0 || !field) {
+    return (
+      <div
+        key={key}
+        className="bg-gray-100 text-sm focus:bg-gray-100 border-gray-200 border-r border-b py-1 px-2"
+        style={style}
+      />
+    );
   }
 
   return (
-    <div key={`row-${rowIndex}-column-${columnIndex}`} className="single-line bg-gray-100 text-sm truncate focus:bg-gray-100 border-r border-gray-200 items-center py-1 px-2" style={style}>
+    <div
+      key={key}
+      className="single-line bg-gray-100 text-sm truncate focus:bg-gray-100 border-r border-gray-200 items-center py-1 px-2"
+      style={style}
+    >
       <FieldTypeIcon typeId={field.fieldTypeId} fieldTypes={fieldTypes} className="mr-1" />
       <span>{field.name}</span>
     </div>
   );
 }
+
+CellRenderer.propTypes = {
+  rowIndex: PropTypes.number.isRequired,
+  columnIndex: PropTypes.number.isRequired,
+  field: PropTypes.object,
+  fieldTypes: PropTypes.array.isRequired,
+  style: PropTypes.object,
+};
 
 export function TableHeader({ scrollLeft, width, hasScrollbar }) {
   const { data: fields } = useViewFields();
@@ -27,28 +52,29 @@ export function TableHeader({ scrollLeft, width, hasScrollbar }) {
   return (
     <Grid
       scrollLeft={scrollLeft}
-      width={hasScrollbar ? width - 15 : width}
+      rowCount={1}
+      columnCount={fields.length + 1}
+      columnWidth={({ index }) => {
+        if (index === 0) return ROW_NO_CELL_WIDTH;
+        if (fields[index - 1]) return fields[index - 1].width;
+        return DEFAULT_CELL_WIDTH;
+      }}
+      rowHeight={30}
+      height={30}
+      width={hasScrollbar ? width - SCROLLBAR_WIDTH : width}
+      className="scrollbar-none border-gray-200 border-b"
       cellRenderer={({ columnIndex, ...props }) => cellRenderer({
         ...props,
         columnIndex,
         field: fields[columnIndex - 1],
         fieldTypes,
       })}
-      rowCount={1}
-      columnCount={fields.length + 1}
-      columnWidth={({ index }) => {
-        if (index === 0) {
-          return 80;
-        }
-        if (fields[index - 1]?.width) {
-          return fields[index - 1].width;
-        }
-
-        return 300;
-      }}
-      rowHeight={30}
-      height={30}
-      className="scrollbar-none border-gray-200 border-b"
     />
   );
 }
+
+TableHeader.propTypes = {
+  scrollLeft: PropTypes.number,
+  width: PropTypes.number,
+  hasScrollbar: PropTypes.bool,
+};
