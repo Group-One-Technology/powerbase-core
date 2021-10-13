827abae106e4a78dc8d6b98c1433570a057d78c7
Joeylene Rivera <joeylenerivera@gmail.com>
2021-10-13 22:44:31 +0800

    fix: update condition for pii abbreviation

--- powerbase-server/app/jobs/powerbase_database_migration_job.rb
+++ powerbase-server/app/jobs/powerbase_database_migration_job.rb
@@ -393,7 +393,7 @@ class PowerbaseDatabaseMigrationJob < ApplicationJob
       data = data.to_s.downcase.gsub("_"," ")
       piis.each do |pii|
         return true if data.include?(pii.name.downcase)
-        return true if pii.abbreviation &&  data.include?(pii.abbreviation.downcase)
+        return true if pii.abbreviation != nil && pii.abbreviation.length > 0 && data.include?(pii.abbreviation.downcase)
       end
 
       false
