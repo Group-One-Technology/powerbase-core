60dd71c92e98c8b2e2de8f17f7335a2f365b6714
Joeylene Rivera <joeylenerivera@gmail.com>
2021-10-19 18:46:21 +0800

    feat: add refresh interval to get databases

--- powerbase-client/src/models/Bases.jsx
+++ powerbase-client/src/models/Bases.jsx
@@ -1,17 +1,31 @@
+import { useEffect, useState } from 'react';
 import constate from 'constate';
 import useSWR from 'swr';
 
 import { getDatabases } from '@lib/api/databases';
 import { useAuthUser } from './AuthUser';
 
+const REFRESH_INTERVAL = 5000; // milliseconds = 5 seconds
+
 function useBasesModel() {
   const { authUser } = useAuthUser();
+  const [refreshInterval, setRefeshInterval] = useState(0);
 
   const response = useSWR(
     authUser ? '/databases' : null,
     getDatabases,
-    { revalidateOnFocus: true },
+    { revalidateOnFocus: true, refreshInterval },
   );
+
+  useEffect(() => {
+    const migratingNonTurboBases = response.data?.filter((item) => !item.isMigrated && !item.isTurbo);
+
+    if (migratingNonTurboBases?.length > 0 && refreshInterval === 0) {
+      setRefeshInterval(REFRESH_INTERVAL);
+    } else if (refreshInterval > 0) {
+      setRefeshInterval(0);
+    }
+  }, [response.data]);
 
   return {
     ...response,
