538273784dc19d8c7952b569c5dd743255744bc9
Francis Bulus <notchera@gmail.com>
2021-10-25 06:35:31 +0100

    add debounce resolver helper

---
+++ powerbase-client/src/lib/helpers/promises/debounceResolver.js
@@ -0,0 +1,70 @@
+/* eslint-disable */
+import DebouncePromise from "./debounce";
+import onlyResolvesLast from "./resolveLastPromise";
+
+const DefaultOptions = {
+  key() {
+    let _args = [];
+    for (let _i = 0; _i < arguments.length; _i++) {
+      _args[_i] = arguments[_i];
+    }
+    return null;
+  },
+  onlyResolvesLast: true,
+};
+
+const DebounceCache = (function () {
+  class DebounceCache {
+    constructor(config) {
+      this.config = config;
+      this.debounceSingleton = null;
+      this.debounceCache = {};
+    }
+    _createDebouncedFunction() {
+      let debouncedFunc = DebouncePromise(
+        this.config.func,
+        this.config.wait,
+        this.config.options
+      );
+      if (this.config.options.onlyResolvesLast) {
+        debouncedFunc = onlyResolvesLast(debouncedFunc);
+      }
+      return {
+        func: debouncedFunc,
+      };
+    }
+    getDebouncedFunction(args) {
+      let _a;
+      let key = (_a = this.config.options).key.apply(_a, args);
+      if (key === null || typeof key === "undefined") {
+        if (!this.debounceSingleton) {
+          this.debounceSingleton = this._createDebouncedFunction();
+        }
+        return this.debounceSingleton;
+      }
+      if (!this.debounceCache[key]) {
+        this.debounceCache[key] = this._createDebouncedFunction();
+      }
+      return this.debounceCache[key];
+    }
+  }
+  return DebounceCache;
+})();
+
+export default function debounceResolve(func, wait, options) {
+  const finalOptions = { ...DefaultOptions, ...options };
+  const debounceCache = new DebounceCache({
+    func,
+    wait,
+    options: finalOptions,
+  });
+  const debounceResolveWrapper = function () {
+    const args = [];
+    for (let _i = 0; _i < arguments.length; _i++) {
+      args[_i] = arguments[_i];
+    }
+    const debouncedFn = debounceCache.getDebouncedFunction(args).func;
+    return debouncedFn.apply(void 0, args);
+  };
+  return debounceResolveWrapper;
+}
