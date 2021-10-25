b64229d0c5161888bd3b3e94cdb43a1d2c4e9ddc
Francis Bulus <notchera@gmail.com>
2021-10-25 05:44:20 +0100

    add helper for creating imperative promises

---
+++ powerbase-client/src/lib/helpers/promises/imperativePromise.js
@@ -0,0 +1,32 @@
+/* eslint-disable */
+
+export default function createImperativePromise(promiseArg) {
+  let resolve = null;
+  let reject = null;
+  const wrappedPromise = new Promise((_resolve, _reject) => {
+    resolve = _resolve;
+    reject = _reject;
+  });
+  promiseArg &&
+    promiseArg.then(
+      (val) => {
+        resolve && resolve(val);
+      },
+      (error) => {
+        reject && reject(error);
+      }
+    );
+  return {
+    promise: wrappedPromise,
+    resolve(value) {
+      resolve && resolve(value);
+    },
+    reject(reason) {
+      reject && reject(reason);
+    },
+    cancel() {
+      resolve = null;
+      reject = null;
+    },
+  };
+}
