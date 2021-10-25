6d6c5670872ef32b0b7f27c6c99778357e2d2c1b
Francis Bulus <notchera@gmail.com>
2021-10-25 05:55:53 +0100

    add resolve last promise helper

---
+++ powerbase-client/src/lib/helpers/promises/resolveLastPromise.js
@@ -0,0 +1,20 @@
+/* eslint-disable */
+import createImperativePromise from "./imperativePromise";
+
+export default function onlyResolvesLast(asyncFunction) {
+  let cancelPrevious = null;
+  const wrappedFunction = () => {
+    const args = [];
+    for (const i = 0; i < arguments.length; i++) {
+      args[i] = arguments[i];
+    }
+    cancelPrevious && cancelPrevious();
+    const initialPromise = asyncFunction.apply(void 0, args);
+    const imperativePromise = createImperativePromise(initialPromise);
+    const { promise } = imperativePromise;
+    const { cancel } = imperativePromise;
+    cancelPrevious = cancel;
+    return promise;
+  };
+  return wrappedFunction;
+}
