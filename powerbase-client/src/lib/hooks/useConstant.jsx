4d217e0ecd4e8eaa3d817409993fac9c4717c4be
Francis Bulus <notchera@gmail.com>
2021-10-25 05:26:03 +0100

    set up useconstant hook

--- powerbase-client/src/lib/hooks/useConstant.jsx
+++ powerbase-client/src/lib/hooks/useConstant.jsx
@@ -1,11 +1,10 @@
+/* eslint-disable quotes */
 import * as React from "react";
 
-export default function useConstant() {
+export default function useConstant(fn) {
   const ref = React.useRef();
-
   if (!ref.current) {
     ref.current = { v: fn() };
   }
-
   return ref.current.v;
 }
