825bb4f9be70772203111b1f0eb8085862fb97b6
Joeylene Rivera <joeylenerivera@gmail.com>
2021-10-27 22:09:09 +0800

    feat: add collaborators icon to base item

--- powerbase-client/src/components/bases/BaseItem.jsx
+++ powerbase-client/src/components/bases/BaseItem.jsx
@@ -1,27 +1,45 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
-import { ChevronRightIcon, ExclamationCircleIcon, LightningBoltIcon } from '@heroicons/react/outline';
+import {
+  ChevronRightIcon,
+  ExclamationCircleIcon,
+  LightningBoltIcon,
+  UserIcon,
+} from '@heroicons/react/outline';
 import * as Tooltip from '@radix-ui/react-tooltip';
 
 import { IBase } from '@lib/propTypes/base';
 import { Badge } from '@components/ui/Badge';
 
 export function BaseItem({ base, handleErrorClick }) {
+  const totalCollaborators = 0;
+
   return (
     <div className="relative p-2 h-full flex flex-col justify-center">
-      {base.isTurbo && (
-        <Tooltip.Root delayDuration={0}>
-          <Tooltip.Trigger className="absolute top-1 right-1 bg-gray-200 rounded">
-            <span className="sr-only">Turbo Mode</span>
-            <LightningBoltIcon className="h-4 w-4 text-gray-500" />
-          </Tooltip.Trigger>
-          <Tooltip.Content className="bg-gray-900 text-white text-xs p-1 rounded">
-            <Tooltip.Arrow className="gray-900" />
-            Turbo Mode
-          </Tooltip.Content>
-        </Tooltip.Root>
-      )}
+      <div className="absolute top-1 right-1 flex gap-1.5">
+        {base.isTurbo && (
+          <Tooltip.Root delayDuration={0}>
+            <Tooltip.Trigger className="py-[1px] px-0.5 bg-gray-100 rounded text-gray-500">
+              <span className="sr-only">Turbo Mode</span>
+              <LightningBoltIcon className="h-4 w-4" />
+            </Tooltip.Trigger>
+            <Tooltip.Content className="py-1 px-2 bg-gray-900 text-white text-xs rounded">
+              <Tooltip.Arrow className="gray-900" />
+              Turbo Mode
+            </Tooltip.Content>
+          </Tooltip.Root>
+        )}
+        {totalCollaborators > 0 && (
+          <button
+            type="button"
+            className="py-[1px] px-0.5 flex items-center bg-gray-100 rounded text-sm text-gray-500"
+          >
+            {totalCollaborators}
+            <UserIcon className="h-4 w-4" />
+          </button>
+        )}
+      </div>
       <Link to={`/base/${base.id}`}>
         <h2 className="text-gray-900 text-lg font-bold uppercase break-words" style={{ hyphens: 'auto' }}>{base.name}</h2>
         {base.isMigrated
