c56f6c34e01a9382c78ef570da7f15f6f536bb33
Joeylene Rivera <joeylenerivera@gmail.com>
2021-10-27 21:52:52 +0800

    feat: add turbo tooltip to base item

--- powerbase-client/src/components/bases/BaseItem.jsx
+++ powerbase-client/src/components/bases/BaseItem.jsx
@@ -1,24 +1,37 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
-import { ChevronRightIcon, ExclamationCircleIcon } from '@heroicons/react/outline';
+import { ChevronRightIcon, ExclamationCircleIcon, LightningBoltIcon } from '@heroicons/react/outline';
+import * as Tooltip from '@radix-ui/react-tooltip';
 
 import { IBase } from '@lib/propTypes/base';
 import { Badge } from '@components/ui/Badge';
 
 export function BaseItem({ base, handleErrorClick }) {
   return (
-    <div className="p-8 h-full flex flex-col justify-center">
+    <div className="relative p-2 h-full flex flex-col justify-center">
+      {base.isTurbo && (
+        <Tooltip.Root delayDuration={0}>
+          <Tooltip.Trigger className="absolute top-1 right-1 bg-gray-200 rounded">
+            <span className="sr-only">Turbo Mode</span>
+            <LightningBoltIcon className="h-4 w-4 text-gray-500" />
+          </Tooltip.Trigger>
+          <Tooltip.Content className="bg-gray-900 text-white text-xs p-1 rounded">
+            <Tooltip.Arrow className="gray-900" />
+            Turbo Mode
+          </Tooltip.Content>
+        </Tooltip.Root>
+      )}
       <Link to={`/base/${base.id}`}>
-        <h2 className="mt-3 text-gray-900 text-xl font-bold uppercase break-words" style={{ hyphens: 'auto' }}>{base.name}</h2>
+        <h2 className="text-gray-900 text-lg font-bold uppercase break-words" style={{ hyphens: 'auto' }}>{base.name}</h2>
         {base.isMigrated
           ? (
             <>
               <dl className="mt-1">
-                <dt className="sr-only">No. of Tables</dt>
-                <dd className="text-gray-500 text-sm">{base.totalTables} tables</dd>
+                <dt className="sr-only text-sm">No. of Tables</dt>
+                <dd className="text-gray-500 text-xs">{base.totalTables} tables</dd>
               </dl>
-              <p className="mt-1 font-medium text-sm text-indigo-600 hover:text-indigo-500 inline-flex items-center">
+              <p className="mt-1 font-medium text-xs text-indigo-600 hover:text-indigo-500 inline-flex items-center">
                 View
                 <ChevronRightIcon className="ml-1 h-4 w-4" />
               </p>
