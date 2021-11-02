36a7fcb0bcd05369e2f689650bd6a97a0f913a35
Joeylene Rivera <joeylenerivera@gmail.com>
2021-11-02 17:16:02 +0800

    refactor: update button loading state

--- powerbase-client/src/components/ui/Button.jsx
+++ powerbase-client/src/components/ui/Button.jsx
@@ -9,11 +9,12 @@ export const Button = React.forwardRef(({
   ...props
 }, ref) => (
   <button ref={ref} disabled={disabled || loading} {...props}>
-    {children}
+    {!loading && children}
     {loading && (
       <>
+        <span className="invisible">{children}</span>
         <span className="sr-only">Loading.</span>
-        <Spinner className="h-4 w-4 ml-1 text-current" />
+        <Spinner className="absolute h-4 w-4 ml-1 text-current" />
       </>
     )}
   </button>
