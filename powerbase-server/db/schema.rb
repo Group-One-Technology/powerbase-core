0a7a049cf782eccb95ed298c6186aa3c1b880faa
Joeylene Rivera <joeylenerivera@gmail.com>
2021-11-03 16:17:48 +0800

    feat: add is_accepted column to guests

--- powerbase-server/db/schema.rb
+++ powerbase-server/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_29_113745) do
+ActiveRecord::Schema.define(version: 2021_11_03_080759) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -61,6 +61,7 @@ ActiveRecord::Schema.define(version: 2021_10_29_113745) do
     t.bigint "powerbase_database_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "is_accepted", default: false
     t.index ["powerbase_database_id"], name: "index_guests_on_powerbase_database_id"
     t.index ["user_id"], name: "index_guests_on_user_id"
   end
