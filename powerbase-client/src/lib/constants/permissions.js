7e0bdf9bc1847f33f23168913d4d55b247bf16c1
Joeylene Rivera <joeylenerivera@gmail.com>
2021-11-09 16:35:28 +0800

    feat: add base permissions and update descriptions

--- powerbase-client/src/lib/constants/permissions.js
+++ powerbase-client/src/lib/constants/permissions.js
@@ -41,30 +41,105 @@ export const CUSTOM_SIMPLE_PERMISSIONS = [
 ];
 
 export const CUSTOM_PERMISSIONS = {
+  Base: [
+    {
+      name: 'Manage Base',
+      description: 'who can configure the base remote DB connection.',
+      value: { manageBase: true },
+      defaultValue: false,
+    },
+    {
+      name: 'Invite Guests',
+      description: 'who can send invites to users.',
+      value: { inviteGuests: true },
+      defaultValue: false,
+    },
+    {
+      name: 'Change Guest Access',
+      description: 'who can change guest\'s access level and permissions.',
+      value: { changeGuestAccess: true },
+      defaultValue: false,
+    },
+    {
+      name: 'Remove Guest Access',
+      description: 'who can remove a guest from this base.',
+      value: { removeGuestAccess: true },
+      defaultValue: false,
+    },
+    {
+      name: 'Add Tables',
+      value: { addTables: true },
+      description: 'who can create a table for this base.',
+      defaultValue: true,
+    },
+    {
+      name: 'Delete Tables',
+      value: { deleteTables: true },
+      description: 'who can drop a table for this base.',
+      defaultValue: false,
+    },
+  ],
   Table: [
     {
       name: 'View Table',
       description: 'who can see this table.',
       value: { viewTable: true },
+      defaultValue: true,
     },
     {
       name: 'Manage Table',
       description: 'who can update table alias, reorder them, etc.',
       value: { manageTable: true },
+      defaultValue: false,
+    },
+    {
+      name: 'Add Views',
+      value: { addViews: true },
+      description: 'who can create collaborative views for this table.',
+      defaultValue: true,
+    },
+    {
+      name: 'Manage Views',
+      description: 'who can update the filter, sort, resize/reorder fields, etc.',
+      value: { manageViews: true },
+      defaultValue: true,
+    },
+    {
+      name: 'Delete Views',
+      value: { deleteViews: true },
+      description: 'who can delete collaborative views for this table.',
+      defaultValue: true,
     },
     {
       name: 'Add Records',
       value: { addRecords: true },
+      description: 'who can add and edit records for this table.',
+      defaultValue: true,
     },
     {
       name: 'Delete Records',
       value: { deleteRecords: true },
+      description: 'who can delete records for this table.',
+      defaultValue: true,
+    },
+    {
+      name: 'Add Fields',
+      value: { addFields: true },
+      description: 'who can create an actual field and magic field for this table.',
+      defaultValue: true,
+    },
+    {
+      name: 'Delete Fields',
+      value: { deleteFields: true },
+      description: 'who can drop a column/field for this table.',
+      defaultValue: false,
     },
     {
       name: 'Comment Records',
       description: 'who can add comments to the records.',
       value: { commentRecords: true },
       disabled: true,
+      defaultValue: true,
     },
   ],
   Field: [
@@ -72,16 +147,19 @@ export const CUSTOM_PERMISSIONS = {
       name: 'View Field',
       description: 'who can see this field.',
       value: { viewField: true },
+      defaultValue: true,
     },
     {
       name: 'Manage Field',
-      description: 'who can update the field type, set as PII, etc.',
+      description: 'who can update the alias and/or field type, set as PII, etc.',
       value: { manageField: true },
+      defaultValue: false,
     },
     {
       name: 'Edit Field Data',
       description: 'who can update this specific field of the records.',
       value: { editFieldData: true },
+      defaultValue: true,
     },
   ],
 };
