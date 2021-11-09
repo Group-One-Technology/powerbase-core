4fed5bebe1be9b49c860c62a0bf364c62a0f8be3
Joeylene Rivera <joeylenerivera@gmail.com>
2021-11-09 15:50:58 +0800

    refactor: update list of table permissions

--- powerbase-client/src/components/guest/TablePermissions.jsx
+++ powerbase-client/src/components/guest/TablePermissions.jsx
@@ -51,12 +51,12 @@ export function TablePermissions({ guest }) {
       <div className="flex items-center gap-1 my-2">
         <h5 className="flex-1 text-base font-medium text-gray-900">Table</h5>
         <Listbox value={table} onChange={setTable} disabled={!changeGuestAccess}>
-          <div className="flex-1">
+          <div className="flex-1 relative">
             <Listbox.Button
               className="block relative w-full text-sm capitalize h-8 px-2 py-1 text-left border border-gray-300 bg-white rounded-md cursor-default focus:outline-none focus-visible:ring-2 focus-visible:ring-indigo-500 focus-visible:border-indigo-500 sm:text-sm"
               disabled={!changeGuestAccess}
             >
-              <span className="block truncate">{table.name}</span>
+              <span className="block truncate">{table.alias || table.name}</span>
               <span className="absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
                 <SelectorIcon
                   className="w-5 h-5 text-gray-400"
@@ -64,7 +64,7 @@ export function TablePermissions({ guest }) {
                 />
               </span>
             </Listbox.Button>
-            <Listbox.Options className="absolute z-10 mt-1 w-auto text-left bg-white shadow-lg max-h-60 rounded-md py-1 text-sm ring-1 ring-black ring-opacity-5 overflow-auto focus:outline-none sm:text-sm">
+            <Listbox.Options className="absolute z-10 mt-1 w-full text-left bg-white shadow-lg max-h-60 rounded-md py-1 text-sm ring-1 ring-black ring-opacity-5 overflow-auto focus:outline-none sm:text-sm">
               {tables.map((option) => (
                 <Listbox.Option
                   key={option.id}
@@ -74,7 +74,7 @@ export function TablePermissions({ guest }) {
                     (active || selected) ? 'bg-gray-100' : 'bg-white',
                   )}
                 >
-                  {option.name}
+                  {option.alias || option.name}
                 </Listbox.Option>
               ))}
             </Listbox.Options>
@@ -87,30 +87,34 @@ export function TablePermissions({ guest }) {
           const itemKey = item.name.split(' ').join('_');
 
           return (
-            <li key={itemKey} className="my-1 flex text-sm text-gray-900">
-              <label htmlFor={itemKey}>
-                {item.name}
-              </label>
-              <Switch
-                id={itemKey}
-                checked={item.enabled}
-                onChange={() => handlePermissionToggle(item)}
-                className={cn(
-                  'ml-auto relative inline-flex flex-shrink-0 h-4 w-7 border-2 border-transparent rounded-full transition-colors ease-in-out duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500',
-                  item.enabled ? 'bg-indigo-600' : 'bg-gray-200',
-                  (loading || !canToggleAccess) ? 'cursor-not-allowed' : 'cursor-pointer',
-                )}
-                disabled={loading || !canToggleAccess}
-              >
-                <span className="sr-only">Show Field</span>
-                <span
-                  aria-hidden="true"
+            <li key={itemKey} className="my-2">
+              <label className="flex w-full">
+                <div>
+                  <div className="font-medium text-sm capitalize">
+                    {item.name}
+                  </div>
+                  <p className="text-xs text-gray-500">{item.description}</p>
+                </div>
+                <Switch
+                  checked={item.enabled}
+                  onChange={() => handlePermissionToggle(item)}
                   className={cn(
-                    'pointer-events-none inline-block h-3 w-3 rounded-full bg-white shadow transform ring-0 transition ease-in-out duration-200',
-                    item.enabled ? 'translate-x-3' : 'translate-x-0',
+                    'ml-auto relative inline-flex flex-shrink-0 h-4 w-7 border-2 border-transparent rounded-full transition-colors ease-in-out duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500',
+                    item.enabled ? 'bg-indigo-600' : 'bg-gray-200',
+                    (loading || !canToggleAccess) ? 'cursor-not-allowed' : 'cursor-pointer',
                   )}
-                />
-              </Switch>
+                  disabled={loading || !canToggleAccess}
+                >
+                  <span className="sr-only">Show Field</span>
+                  <span
+                    aria-hidden="true"
+                    className={cn(
+                      'pointer-events-none inline-block h-3 w-3 rounded-full bg-white shadow transform ring-0 transition ease-in-out duration-200',
+                      item.enabled ? 'translate-x-3' : 'translate-x-0',
+                    )}
+                  />
+                </Switch>
+              </label>
             </li>
           );
         })}
