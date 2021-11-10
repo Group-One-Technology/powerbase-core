f4b41d8957737d9a1228640f72996956e811a07e
Joeylene Rivera <joeylenerivera@gmail.com>
2021-11-11 02:41:15 +0800

    refactor: remove see_view permission

--- powerbase-server/app/helpers/permissions_helper.rb
+++ powerbase-server/app/helpers/permissions_helper.rb
@@ -2,15 +2,15 @@ module PermissionsHelper
   BASE_PERMISSIONS = [:view_base, :manage_base, :invite_guests, :change_guest_access, :remove_guests, :add_tables, :delete_tables]
   TABLE_PERMISSIONS = [:view_table, :manage_table, :add_views, :delete_views, :add_fields, :delete_fields, :add_records, :delete_records, :comment_records]
   FIELD_PERMISSIONS = [:view_field, :manage_field, :edit_field_data]
-  VIEW_PERMISSIONS = [:see_view, :manage_view]
+  VIEW_PERMISSIONS = [:manage_view]
   PERMISSIONS = [*BASE_PERMISSIONS, *TABLE_PERMISSIONS, *FIELD_PERMISSIONS, *VIEW_PERMISSIONS]
 
   ROLES = {
     :owner => [:all],
     :admin => [:view_base, *TABLE_PERMISSIONS, *FIELD_PERMISSIONS, *VIEW_PERMISSIONS],
-    :editor => [:view_base, :add_views, *VIEW_PERMISSIONS, :view_table, :view_field, :see_view, :add_records, :delete_records, :comment_records],
-    :commenter => [:view_base, :view_table, :view_field, :see_view, :comment_records],
-    :viewer => [:view_base, :view_table, :view_field, :see_view],
+    :editor => [:view_base, :add_views, *VIEW_PERMISSIONS, :view_table, :view_field, :add_records, :delete_records, :comment_records],
+    :commenter => [:view_base, :view_table, :view_field, :comment_records],
+    :viewer => [:view_base, :view_table, :view_field],
   }
 
   class NotFound < StandardError
