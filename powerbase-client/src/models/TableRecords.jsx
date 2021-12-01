44b925e41ddd7cca6c9a64e9020bd6c8ef746abf
Francis Bulus <notchera@gmail.com>
2021-12-01 13:24:21 +0100

    _

--- powerbase-client/src/models/TableRecords.jsx
+++ powerbase-client/src/models/TableRecords.jsx
@@ -6,7 +6,7 @@ import { getTableRecords, getMagicValues } from "@lib/api/records";
 import { useAuthUser } from "./AuthUser";
 import { useViewOptions } from "./views/ViewOptions";
 
-function getKey({ index, tableId, query, sort, filters, pageSize, isTurbo }) {
+function getKey({ index, tableId, query, sort, filters, pageSize }) {
   const page = index + 1;
   const pageQuery = `page=${page}&limit=${pageSize}`;
   const searchQuery = query?.length ? `&q=${encodeURIComponent(query)}` : "";
@@ -16,7 +16,7 @@ function getKey({ index, tableId, query, sort, filters, pageSize, isTurbo }) {
   return `/tables/${tableId}/records?${pageQuery}${searchQuery}${filterQuery}${sortQuery}`;
 }
 
-function useTableRecordsModel({ id, pageSize = 40, base }) {
+function useTableRecordsModel({ id, pageSize = 40, isTurbo }) {
   const { authUser } = useAuthUser();
   const { viewId, query, filters, sort } = useViewOptions();
 
@@ -50,37 +50,35 @@ function useTableRecordsModel({ id, pageSize = 40, base }) {
 
   const { data, error, size, setSize } = response;
 
-  const magicValuesResponse = useSWR(
-    `/tables/${id}/magic_values`,
-    getMagicValues
-  );
-
-  const { data: magicData } = magicValuesResponse;
-
   let parsedData = data && data?.reduce((prev, cur) => prev?.concat(cur), []);
-  let magicValues = magicData;
-  console.log(magicData);
-
   let mergedData = parsedData;
-  parsedData?.forEach((record, idx) =>
-    magicValues?.forEach((magicValue) => {
-      const formattedPkFieldNameLabel = `PrimaryKeyNameTable${id}`;
-      const formattedPkFieldValueLabel = `PrimaryKeyValueTable${id}`;
-      const pkName = magicValue[formattedPkFieldNameLabel];
-      const pkValue = magicValue[formattedPkFieldValueLabel];
-      if (record[pkName] === pkValue) {
-        const {
-          formattedPkFieldNameLabel,
-          formattedPkFieldValueLabel,
-          ...associatedValue
-        } = magicValue;
-        const combined = { ...record, ...associatedValue };
-        mergedData[idx] = combined;
-      }
-    })
-  );
 
-  console.log(mergedData);
+  if (!isTurbo) {
+    const magicValuesResponse = useSWR(
+      `/tables/${id}/magic_values`,
+      getMagicValues
+    );
+
+    const { data: magicData } = magicValuesResponse;
+    parsedData?.forEach((record, idx) =>
+      magicData?.forEach((magicValue) => {
+        const formattedPkFieldNameLabel = `PrimaryKeyNameTable${id}`;
+        const formattedPkFieldValueLabel = `PrimaryKeyValueTable${id}`;
+        const pkName = magicValue[formattedPkFieldNameLabel];
+        const pkValue = magicValue[formattedPkFieldValueLabel];
+        if (record[pkName] === pkValue) {
+          const {
+            formattedPkFieldNameLabel,
+            formattedPkFieldValueLabel,
+            ...associatedValue
+          } = magicValue;
+          const combined = { ...record, ...associatedValue };
+          mergedData[idx] = combined;
+        }
+      })
+    );
+  }
+
   const isLoadingInitialData = !data && !error;
   const isLoading =
     isLoadingInitialData ||
