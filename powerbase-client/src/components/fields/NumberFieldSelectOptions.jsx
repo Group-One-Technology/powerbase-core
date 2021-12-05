537878a5366a9eefb610e9b6c27d2609378d28c2
Francis Bulus <notchera@gmail.com>
2021-12-05 20:59:53 +0100

    add proptype validation to number field selection component

--- powerbase-client/src/components/fields/NumberFieldSelectOptions.jsx
+++ powerbase-client/src/components/fields/NumberFieldSelectOptions.jsx
@@ -1,15 +1,15 @@
-/* eslint-disable */
-import React, { Fragment, useState, useEffect } from "react";
-import { Listbox, Transition } from "@headlessui/react";
-import { CheckIcon, SelectorIcon } from "@heroicons/react/solid";
+import React, { Fragment, useState, useEffect } from 'react';
+import { Listbox, Transition } from '@headlessui/react';
+import { CheckIcon, SelectorIcon } from '@heroicons/react/solid';
 import {
   CURRENCY_OPTIONS,
   NUMBER_FORMAT_OPTIONS,
   PRECISION_POINTS,
-} from "@lib/constants";
+} from '@lib/constants';
+import PropTypes from 'prop-types';
 
 function classNames(...classes) {
-  return classes.filter(Boolean).join(" ");
+  return classes.filter(Boolean).join(' ');
 }
 
 export default function NumberFieldSelectOptions({
@@ -24,35 +24,35 @@ export default function NumberFieldSelectOptions({
     if (isPrecision) {
       if (isPercent || isCurrency) {
         return PRECISION_POINTS[0];
-      } else return PRECISION_POINTS[1];
-    } else if (isCurrency) {
+      } return PRECISION_POINTS[1];
+    } if (isCurrency) {
       return CURRENCY_OPTIONS[0];
-    } else return NUMBER_FORMAT_OPTIONS[0];
+    } return NUMBER_FORMAT_OPTIONS[0];
   };
 
-  const [selected, setSelected] = useState(computeInitialSelected());
+  const [selectedItem, setSelectedItem] = useState(computeInitialSelected());
   const hasIntPrecisionOption = isPercent || isCurrency;
   const handleSelect = (item) => {
     if (isPrecision) {
       setNumberPrecision(item);
-      setSelected(item);
+      setSelectedItem(item);
     } else if (isCurrency && setCurrency) {
       setCurrency(item.code);
-      setSelected(item);
+      setSelectedItem(item);
     } else {
       setNumberSubtype(item);
-      setSelected(item);
+      setSelectedItem(item);
     }
   };
 
   useEffect(() => {
-    if (!isPrecision) setNumberSubtype(selected);
-    if (isPrecision) setNumberPrecision(selected);
-    if (isCurrency && setCurrency) setCurrency(selected.code);
-  }, [selected]);
+    if (!isPrecision) setNumberSubtype(selectedItem);
+    if (isPrecision) setNumberPrecision(selectedItem);
+    if (isCurrency && setCurrency) setCurrency(selectedItem.code);
+  }, [selectedItem]);
 
   return (
-    <Listbox value={selected} onChange={(item) => handleSelect(item)}>
+    <Listbox value={selectedItem} onChange={(item) => handleSelect(item)}>
       {!isPrecision && (
         <Listbox.Label className="block text-sm font-normal text-gray-700">
           Format
@@ -65,7 +65,7 @@ export default function NumberFieldSelectOptions({
       )}
       <div className="mt-1 relative">
         <Listbox.Button className="bg-white relative w-full border border-gray-300 rounded-md shadow-sm pl-3 pr-10 py-2 text-left cursor-default focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
-          <span className="block truncate">{selected.name}</span>
+          <span className="block truncate">{selectedItem.name}</span>
           <span className="absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
             <SelectorIcon
               className="h-5 w-5 text-gray-400"
@@ -86,25 +86,23 @@ export default function NumberFieldSelectOptions({
                 ? PRECISION_POINTS.filter((point) => point.id !== 1)
                 : PRECISION_POINTS
               : isCurrency
-              ? CURRENCY_OPTIONS
-              : NUMBER_FORMAT_OPTIONS
+                ? CURRENCY_OPTIONS
+                : NUMBER_FORMAT_OPTIONS
             ).map((option) => (
               <Listbox.Option
                 key={isCurrency ? option.code : option.id}
-                className={({ active }) =>
-                  classNames(
-                    active ? "text-white bg-indigo-600" : "text-gray-900",
-                    "cursor-default select-none relative py-2 pl-3 pr-9"
-                  )
-                }
+                className={({ active }) => classNames(
+                  active ? 'text-white bg-indigo-600' : 'text-gray-900',
+                  'cursor-default select-none relative py-2 pl-3 pr-9',
+                )}
                 value={option}
               >
                 {({ selected, active }) => (
                   <>
                     <span
                       className={classNames(
-                        selected ? "font-semibold" : "font-normal",
-                        "block truncate"
+                        selected ? 'font-semibold' : 'font-normal',
+                        'block truncate',
                       )}
                     >
                       {option.name}
@@ -113,8 +111,8 @@ export default function NumberFieldSelectOptions({
                     {selected ? (
                       <span
                         className={classNames(
-                          active ? "text-white" : "text-indigo-600",
-                          "absolute inset-y-0 right-0 flex items-center pr-4"
+                          active ? 'text-white' : 'text-indigo-600',
+                          'absolute inset-y-0 right-0 flex items-center pr-4',
                         )}
                       >
                         <CheckIcon className="h-5 w-5" aria-hidden="true" />
@@ -130,3 +128,12 @@ export default function NumberFieldSelectOptions({
     </Listbox>
   );
 }
+
+NumberFieldSelectOptions.propTypes = {
+  isPrecision: PropTypes.bool.isRequired,
+  setNumberPrecision: PropTypes.func.isRequired,
+  setNumberSubtype: PropTypes.func.isRequired,
+  isPercent: PropTypes.bool.isRequired,
+  isCurrency: PropTypes.bool.isRequired,
+  setCurrency: PropTypes.bool.isRequired,
+};
