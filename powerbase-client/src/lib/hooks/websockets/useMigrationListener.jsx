8f78bbff844a8864ff3b516dd823b892c26fa5d9
Joeylene Rivera <joeylenerivera@gmail.com>
2021-12-08 12:12:07 +0800

    feat: add base migration listener

--- powerbase-client/src/lib/hooks/websockets/useMigrationListener.jsx
+++ powerbase-client/src/lib/hooks/websockets/useMigrationListener.jsx
@@ -2,7 +2,11 @@ import { useEffect, useState } from 'react';
 import Pusher from 'pusher-js';
 import { pusher } from '.';
 
-export function useMigrationListener({ logging, databases, mutate }) {
+const DEFAULT_VALUE = {
+  logging: false,
+};
+
+export function useMigrationListener({ logging = false, databases, mutate } = DEFAULT_VALUE) {
   const [migratingBases, setMigratingBases] = useState(databases
     ?.filter((item) => !item.isMigrated)
     .map((item) => ({ ...item, subscribed: false })));
@@ -23,14 +27,20 @@ export function useMigrationListener({ logging, databases, mutate }) {
   };
 
   useEffect(() => {
-    const updatedMigratingBases = databases?.filter((item) => !item.isMigrated);
+    if (databases && mutate) {
+      const updatedMigratingBases = databases?.filter((item) => !item.isMigrated);
 
-    migratingBases?.forEach((base) => {
-      const isMigrated = !updatedMigratingBases.some((item) => item.id === base.id);
-      if (isMigrated) unsubscribe(base.id);
-    });
+      migratingBases?.forEach((base) => {
+        const isMigrated = !updatedMigratingBases.some((item) => item.id === base.id);
+        if (isMigrated) unsubscribe(base.id);
+      });
 
-    updatedMigratingBases?.map((base) => migrationListener(base.id));
-    setMigratingBases(updatedMigratingBases);
+      updatedMigratingBases?.map((base) => migrationListener(base.id));
+      setMigratingBases(updatedMigratingBases);
+    }
   }, [databases]);
+
+  return {
+    migrationListener,
+  };
 }
