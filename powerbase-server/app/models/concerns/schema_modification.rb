f3c3efb0074f07d8ef754993548df2a29642c40a
Joeylene Rivera <joeylenerivera@gmail.com>
2022-01-11 07:26:43 +0800

    fix: enable deletion of all primary keys in table

--- powerbase-server/app/models/concerns/schema_modification.rb
+++ powerbase-server/app/models/concerns/schema_modification.rb
@@ -15,52 +15,51 @@ module SchemaModification
 
     return if primary_keys.sort == current_primary_keys.sort
 
-    if primary_keys.length > 0
-      is_postgresql = self.db.postgresql?
+    is_postgresql = self.db.postgresql?
 
-      indexes = if self.db.postgresql?
-          _sequel.fetch("
-            SELECT ix.relname as index_name,
-              upper(am.amname) AS index_algorithm,
-              indisunique as is_unique,
-              pg_get_indexdef(indexrelid) as index_definition
-            FROM pg_index i
-            JOIN pg_class t ON t.oid = i.indrelid
-            JOIN pg_class ix ON ix.oid = i.indexrelid
-            JOIN pg_namespace n ON t.relnamespace = n.oid
-            JOIN pg_am as am ON ix.relam = am.oid
-            WHERE t.relname = '#{self.name}' AND n.nspname = 'public';
-          ").all
-        else
-          _sequel.fetch("
-            SELECT index_name as index_name,
-              index_type AS index_algorithm,
-              CASE non_unique
-                WHEN 0 THEN 'TRUE'
-                ELSE 'FALSE'
-              END AS is_unique,
-              column_name as column_name
-            FROM information_schema.statistics
-            WHERE table_schema='#{self.db.database_name}' AND table_name='#{self.name}' ORDER BY seq_in_index ASC;
-          ").all
-        end
-
-      key_query = is_postgresql ? "pk" : "primary"
-      primary_key_index = indexes.select {|index| index[:index_name].downcase.include?(key_query) && index[:is_unique]}.first
-
-      if primary_key_index
-        _sequel.alter_table(table_name) do
-          drop_constraint(primary_key_index[:index_name])
-          add_primary_key(primary_keys)
-        end
+    indexes = if self.db.postgresql?
+        _sequel.fetch("
+          SELECT ix.relname as index_name,
+            upper(am.amname) AS index_algorithm,
+            indisunique as is_unique,
+            pg_get_indexdef(indexrelid) as index_definition
+          FROM pg_index i
+          JOIN pg_class t ON t.oid = i.indrelid
+          JOIN pg_class ix ON ix.oid = i.indexrelid
+          JOIN pg_namespace n ON t.relnamespace = n.oid
+          JOIN pg_am as am ON ix.relam = am.oid
+          WHERE t.relname = '#{self.name}' AND n.nspname = 'public';
+        ").all
       else
-        _sequel.alter_table(table_name) do
-          add_primary_key(primary_keys)
-        end
+        _sequel.fetch("
+          SELECT index_name as index_name,
+            index_type AS index_algorithm,
+            CASE non_unique
+              WHEN 0 THEN 'TRUE'
+              ELSE 'FALSE'
+            END AS is_unique,
+            column_name as column_name
+          FROM information_schema.statistics
+          WHERE table_schema='#{self.db.database_name}' AND table_name='#{self.name}' ORDER BY seq_in_index ASC;
+        ").all
       end
 
-      self._sequel.disconnect
+    key_query = is_postgresql ? "pk" : "primary"
+    primary_key_index = indexes.select {|index| index[:index_name].downcase.include?(key_query) && index[:is_unique]}.first
+
+    if primary_key_index
+      _sequel.alter_table(table_name) do
+        drop_constraint(primary_key_index[:index_name])
+      end
     end
+
+    if primary_keys.length > 0
+      _sequel.alter_table(table_name) do
+        add_primary_key(primary_keys)
+      end
+    end
+
+    self._sequel.disconnect
 
     fields.each do |field|
       is_primary_key = primary_keys.include?(field.name.to_sym)
