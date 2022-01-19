69ca3311600f1f4f7659f12da41d3913e4c29794
Joeylene Rivera <joeylenerivera@gmail.com>
2022-01-19 14:30:38 +0800

    refactor: remove transform pii and json data in model.get

--- powerbase-server/app/lib/powerbase/model.rb
+++ powerbase-server/app/lib/powerbase/model.rb
@@ -79,30 +79,7 @@ module Powerbase
         if magic_fields.length > 0
           doc_id = format_doc_id(options[:primary_keys])
           magic_result = get_record(@index, doc_id)
-
-          if magic_result["found"]
-            magic_record = magic_result["_source"]
-
-            # TODO: Remove when primary keys are now indexed for magic values and find_by is now working for to_elasticsearch.
-            if !include_pii
-              magic_record = magic_record.select do |key, value|
-                field = magic_fields.find {|field| field.name == key.to_s}
-                field ? !field.is_pii : false
-              end
-            end
-
-            if !include_json
-              json_text = PowerbaseFieldType.find_by(name: "JSON Text")
-              magic_record = magic_record.map do |key, value|
-                field = magic_fields.find {|field| field.name == key.to_s}
-                next value[0..40] if field.powerbase_field_type_id == json_text.id
-                next value
-              end
-
-            end
-
-            { **record, **magic_record }
-          end
+          { **record, **magic_result["_source"] }
         else
           record
         end
