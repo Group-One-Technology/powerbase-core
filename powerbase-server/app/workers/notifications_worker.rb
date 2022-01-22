2d91ed3beb8eaff76422027349436b9e7bc9aa22
Joeylene Rivera <joeylenerivera@gmail.com>
2022-01-22 20:31:43 +0800

    fix: relocate notifications cleanup worker

--- powerbase-server/app/workers/notifications_worker.rb
+++ powerbase-server/app/workers/notifications_worker.rb
@@ -2,6 +2,6 @@ class NotificationsWorker
   include Sidekiq::Worker
 
   def perform
-    Notifications.where(has_read: true, created_at: 1.day.ago).destroy_all
+    Notification.where(has_read: true, created_at: 1.day.ago).destroy_all
   end
 end
--- powerbase-server/config/initializers/powerbase_listener.rb
+++ powerbase-server/config/initializers/powerbase_listener.rb
@@ -1,36 +1,38 @@
-if defined?(Rails::Server) && ENV["ENABLE_LISTENER"] == "true"
-  puts "** STARTING DATABASE AUTOSYNC AND LISTENERS **"
-  ids = PowerbaseDatabase.turbo.select(&:has_row_oid_support?).map(&:id)
-
+if defined?(Rails::Server)
   # Destroy existing cron job to avoid duplicate
   Sidekiq::Cron::Job.destroy_all!
-
-  job = Sidekiq::Cron::Job.new(
-    name: "Database Listeners",
-    args: ids,
-    cron: '*/5 * * * *', # Run The job every 5 mins
-    class: 'PollWorker'
-  )
-
-  if job.save
-    job.enque!
-    puts "Powerbase Listening to #{ids.count} turbo databases..."
-  else
-    puts "Can't run database listener cron job"
-    raise job.errors
-  end
 
   notifications_job = Sidekiq::Cron::Job.new(
     name: "Read Notifications Cleanup",
     cron: '0 0 * * *', # Daily at 00:00
-    class: 'PollWorker'
+    class: 'NotificationsWorker'
   )
 
   if notifications_job.save
     notifications_job.enque!
-    puts "Cleaning up a read notifications..."
+    puts "Cleaning up read notifications..."
   else
     puts "Can't run read notifications cleanup cron job"
     raise job.errors
+  end
+
+  if ENV["ENABLE_LISTENER"] == "true"
+    puts "** STARTING DATABASE AUTOSYNC AND LISTENERS **"
+    ids = PowerbaseDatabase.turbo.select(&:has_row_oid_support?).map(&:id)
+
+    job = Sidekiq::Cron::Job.new(
+      name: "Database Listeners",
+      args: ids,
+      cron: '*/5 * * * *', # Run The job every 5 mins
+      class: 'PollWorker'
+    )
+
+    if job.save
+      job.enque!
+      puts "Powerbase Listening to #{ids.count} turbo databases..."
+    else
+      puts "Can't run database listener cron job"
+      raise job.errors
+    end
   end
 end
