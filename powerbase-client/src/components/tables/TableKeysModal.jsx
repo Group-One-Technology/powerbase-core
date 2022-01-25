9b12d06654852db106ca36bcb3ed7061cceebeb2
Joeylene Rivera <joeylenerivera@gmail.com>
2022-01-25 15:52:43 +0800

    fix: temporarily disable settings date fields as primary keys

--- powerbase-client/src/components/tables/TableKeysModal.jsx
+++ powerbase-client/src/components/tables/TableKeysModal.jsx
@@ -197,39 +197,45 @@ export function TableKeysModal() {
               Fields
             </h4>
             <ul className="list-none flex flex-col gap-1">
-              {viewFields.map((field) => (
-                <FieldItem
-                  key={field.id}
-                  field={field}
-                  fieldTypes={fieldTypes}
-                  action={(canUpdatePrimaryKey && !field.isPrimaryKey) && (
-                    <>
-                      {field.isPii || field.isVirtual
-                        ? (
-                          <Tooltip.Root delayDuration={0}>
-                            <Tooltip.Trigger className="inline-flex justify-center w-full rounded-md border border-transparent shadow-sm px-2 py-1 text-sm font-medium cursor-not-allowed text-gray-700 bg-gray-200 hover:bg-gray-300 focus:ring-gray-300 focus:outline-none focus:ring-2 focus:ring-offset-2 sm:w-auto">
+              {viewFields.map((field) => {
+                const fieldType = fieldTypes?.find((item) => item.id === field.fieldTypeId);
+
+                return (
+                  <FieldItem
+                    key={field.id}
+                    field={field}
+                    fieldTypes={fieldTypes}
+                    action={(canUpdatePrimaryKey && !field.isPrimaryKey) && (
+                      <>
+                        {field.isPii || field.isVirtual || fieldType?.name === 'Date'
+                          ? (
+                            <Tooltip.Root delayDuration={0}>
+                              <Tooltip.Trigger className="inline-flex justify-center w-full rounded-md border border-transparent shadow-sm px-2 py-1 text-sm font-medium cursor-not-allowed text-gray-700 bg-gray-200 hover:bg-gray-300 focus:ring-gray-300 focus:outline-none focus:ring-2 focus:ring-offset-2 sm:w-auto">
+                                Set as Primary Key
+                              </Tooltip.Trigger>
+                              <Tooltip.Content className="py-1 px-2 bg-gray-900 text-white text-xs rounded">
+                                <Tooltip.Arrow className="gray-900" />
+                                {field.isVirtual
+                                  ? 'Cannot set a Magic field as a Primary Key.'
+                                  : fieldType?.name === 'Date'
+                                    ? 'Temporarily cannot set date fields as Primary Key.'
+                                    : 'Cannot set a PII field as a Primary Key.'}
+                              </Tooltip.Content>
+                            </Tooltip.Root>
+                          ) : (
+                            <button
+                              type="button"
+                              className="inline-flex justify-center w-full rounded-md border border-transparent shadow-sm px-2 py-1 text-sm font-medium cursor-pointer text-gray-900 bg-gray-100 hover:bg-gray-300 focus:ring-gray-300 focus:outline-none focus:ring-2 focus:ring-offset-2 sm:w-auto"
+                              onClick={() => handleSetAsPrimary(field, true)}
+                            >
                               Set as Primary Key
-                            </Tooltip.Trigger>
-                            <Tooltip.Content className="py-1 px-2 bg-gray-900 text-white text-xs rounded">
-                              <Tooltip.Arrow className="gray-900" />
-                              {field.isVirtual
-                                ? 'Cannot set a Magic field as a Primary Key.'
-                                : 'Cannot set a PII field as a Primary Key.'}
-                            </Tooltip.Content>
-                          </Tooltip.Root>
-                        ) : (
-                          <button
-                            type="button"
-                            className="inline-flex justify-center w-full rounded-md border border-transparent shadow-sm px-2 py-1 text-sm font-medium cursor-pointer text-gray-900 bg-gray-100 hover:bg-gray-300 focus:ring-gray-300 focus:outline-none focus:ring-2 focus:ring-offset-2 sm:w-auto"
-                            onClick={() => handleSetAsPrimary(field, true)}
-                          >
-                            Set as Primary Key
-                          </button>
-                        )}
-                    </>
-                  )}
-                />
-              ))}
+                            </button>
+                          )}
+                      </>
+                    )}
+                  />
+                );
+              })}
             </ul>
 
             {tableConnections && tableConnections.length > 0 && (
