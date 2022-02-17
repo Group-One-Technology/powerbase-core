9300847b13a3c176420c0a0bfc8e8b49b1ee0ec9
Joeylene Rivera <joeylenerivera@gmail.com>
2022-02-17 18:14:16 +0800

    feat: add email confirm column migrations to user

--- powerbase-server/db/schema.rb
+++ powerbase-server/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_04_224237) do
+ActiveRecord::Schema.define(version: 2022_02_17_090929) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -204,6 +204,8 @@ ActiveRecord::Schema.define(version: 2022_02_04_224237) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.boolean "is_onboarded", default: false
+    t.boolean "email_confirmed", default: false, null: false
+    t.string "confirm_token"
   end
 
   create_table "view_field_options", force: :cascade do |t|
