0e06ba1002b44d1e8df92ef3eb2b8a2d88595a0c
Joeylene Rivera <joeylenerivera@gmail.com>
2022-02-22 12:23:45 +0800

    refactor: update mailer default email and tags

--- powerbase-server/app/mailers/user_mailer.rb
+++ powerbase-server/app/mailers/user_mailer.rb
@@ -1,13 +1,15 @@
-class UserMailer < ActionMailer::Base
-  default from: "team@editmode.com"
-
+class UserMailer < ApplicationMailer
   def confirm_email(user_id: nil)
     @user = User.find user_id
     raise StandardError.new "Failed to send confirm email, user##{user_id} could not be found." if !@user
-    raise StandardError.new "User##{@user.id}'s email is already confirmed" if !@user.pending_any_confirmation?
+
+    if !@user.pending_any_confirmation?
+      puts "User##{@user.id}'s email is already confirmed"
+      return
+    end
 
     @action_url = "#{ENV["CLIENT"]}/user/confirm-email?token=#{@user.confirmation_token}"
-    @support_email = "team@editmode.com"
+    @support_email = self.class.support_email
 
     mail(
       subject: 'Confirm Your Email',
