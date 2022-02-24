33def0327e4b1b1a811f8d42683680b70ecd73f4
Joeylene Rivera <joeylenerivera@gmail.com>
2022-02-24 09:43:40 +0800

    fix: limit for pg notify for 8000 bytes

--- powerbase-server/app/lib/powerbase/listener.rb
+++ powerbase-server/app/lib/powerbase/listener.rb
@@ -15,7 +15,7 @@ module Powerbase
       @db = powerbase_db._sequel
       begin
         @db.listen("powerbase_table_update", :loop => true) do |ev, pid, payload|
-          notifier_callback(ev, pid, payload)
+          notifier_callback(@db, ev, pid, payload)
         end
       rescue => ex
         puts "#{Time.now} -- Listener Error for Database##{@powerbase_db.id}}"
@@ -23,23 +23,27 @@ module Powerbase
       end
     end
 
-    def notifier_callback(ev, pid, payload)
+    def notifier_callback(database, ev, pid, payload)
       payload_hash = JSON.parse(payload)
       table_name = payload_hash["table"]
       primary_key_value = payload_hash["primary_key"]
       event_type = payload_hash["type"]
-      record = payload_hash["data"]
+      table = database.from(table_name.to_sym)
       powerbase_table = powerbase_db.tables.turbo.find_by name: table_name
 
       index_name = powerbase_table.index_name
+      fields = powerbase_table.fields
       doc_id = format_doc_id(primary_key_value)
 
       puts "#{Time.now} -- Data changes detect on table##{powerbase_table.id} #{table_name}"
 
       case event_type
       when "INSERT"
+        record = table.where(primary_key_value.symbolize_keys).first
+        record = format_record(record, fields)
+
         if !record
-          puts "#{Time.now} -- No inserted record provided in payload"
+          puts "#{Time.now} -- No inserted record found."
           return
         end
 
@@ -49,8 +53,13 @@ module Powerbase
         # Notify changes to client
         pusher_trigger!("table.#{powerbase_table.id}", "powerbase-data-listener", record.merge(doc_id: doc_id))
       when "UPDATE"
+        # Query get record
+        records = sequel_get_records(database, table_name)
+        record = records.where(primary_key_value.symbolize_keys).first
+        record = format_record(record, fields)
+
         if !record
-          puts "#{Time.now} -- No updated record provided in payload"
+          puts "#{Time.now} -- No updated record found."
           return
         end
 
