16c2753300de95b2c383bc16748f4383a0f98233
Joeylene Rivera <joeylenerivera@gmail.com>
2022-03-03 18:35:11 +0800

    fix: check if es result is nil

--- powerbase-server/app/helpers/elasticsearch_helper.rb
+++ powerbase-server/app/helpers/elasticsearch_helper.rb
@@ -156,14 +156,16 @@ module ElasticsearchHelper
   end
 
   def format_es_result(result)
-    result["hits"]["hits"].map do |result|
-      if result["fields"] && result["fields"].length > 0
-        result["fields"].each do |key, value|
-          result["_source"][key] = value[0]
+    if result != nil
+      result["hits"]["hits"].map do |result|
+        if result["fields"] && result["fields"].length > 0
+          result["fields"].each do |key, value|
+            result["_source"][key] = value[0]
+          end
         end
-      end
 
-      result["_source"].symbolize_keys.merge("doc_id": result["_id"])
+        result["_source"].symbolize_keys.merge("doc_id": result["_id"])
+      end
     end
   end
 end
