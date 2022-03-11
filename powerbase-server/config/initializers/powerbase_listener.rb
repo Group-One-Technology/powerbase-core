ef9b25953813b412e0c9b02042f8e5a24735734c
Joeylene Rivera <joeylenerivera@gmail.com>
2022-03-11 21:03:57 +0800

    refactor: add checker if there are db to listen

--- powerbase-server/config/initializers/powerbase_listener.rb
+++ powerbase-server/config/initializers/powerbase_listener.rb
@@ -20,34 +20,36 @@ if defined?(Rails::Server)
     puts "** STARTING DATABASE AUTOSYNC AND LISTENERS **"
     ids = PowerbaseDatabase.turbo.postgresql.ids
 
-    listener_job = Sidekiq::Cron::Job.new(
-      name: "Database Listeners",
-      args: ids,
-      cron: '*/5 * * * *', # Run The job every 5 mins
-      class: 'PollWorker'
-    )
+    if ids.length > 0
+      listener_job = Sidekiq::Cron::Job.new(
+        name: "Database Listeners",
+        args: ids,
+        cron: '*/5 * * * *', # Run The job every 5 mins
+        class: 'PollWorker'
+      )
 
-    if listener_job.save
-      listener_job.enque!
-      puts "Powerbase Listening to #{ids.count} turbo databases..."
-    else
-      puts "Can't run database listener cron job"
-      raise listener_job.errors
-    end
+      if listener_job.save
+        listener_job.enque!
+        puts "Powerbase Listening to #{ids.count} turbo databases..."
+      else
+        puts "Can't run database listener cron job"
+        raise listener_job.errors
+      end
 
-    syncer_job = Sidekiq::Cron::Job.new(
-      name: "Database Syncer",
-      args: ids,
-      cron: '*/15 * * * *', # Run The job every 5 mins
-      class: 'SyncerCronWorker'
-    )
-    
-    if syncer_job.save
-      syncer_job.enque!
-      puts "Powerbase Syncing #{ids.count} turbo databases..."
-    else
-      puts "Can't run database database syncer cron job"
-      raise syncer_job.errors
+      syncer_job = Sidekiq::Cron::Job.new(
+        name: "Database Syncer",
+        args: ids,
+        cron: '*/15 * * * *', # Run The job every 5 mins
+        class: 'SyncerCronWorker'
+      )
+
+      if syncer_job.save
+        syncer_job.enque!
+        puts "Powerbase Syncing #{ids.count} turbo databases..."
+      else
+        puts "Can't run database database syncer cron job"
+        raise syncer_job.errors
+      end
     end
   end
 end
