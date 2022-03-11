c76c4b8917b33a09f38e94daa10f68593d353752
Joeylene Rivera <joeylenerivera@gmail.com>
2022-03-11 14:24:41 +0800

    feat: add write_error_logs to base migration

--- powerbase-server/app/models/base_migration.rb
+++ powerbase-server/app/models/base_migration.rb
@@ -3,4 +3,10 @@ class BaseMigration < ApplicationRecord
   serialize :logs, JSON
 
   belongs_to :powerbase_database
+
+  def write_error_logs!(error)
+    self.logs["errors"] = [] if !self.logs["errors"]
+    self.logs["errors"] << error
+    self.save
+  end
 end
