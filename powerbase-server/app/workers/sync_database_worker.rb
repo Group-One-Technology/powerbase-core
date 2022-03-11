ff88c18bc40004d128a87e01c55da6bf5ac3532b
Joeylene Rivera <joeylenerivera@gmail.com>
2022-03-11 17:11:17 +0800

    refactor: replace database migration job

--- powerbase-server/app/workers/sync_database_worker.rb
+++ powerbase-server/app/workers/sync_database_worker.rb
@@ -1,88 +1,89 @@
-class SyncDatabaseWorker
-  include Sidekiq::Worker
-  include PusherHelper
+include PusherHelper
 
-  attr_accessor :database, :unmigrated_tables, :deleted_tables, :new_connection
+class SyncDatabaseWorker < ApplicationWorker
+  attr_accessor :database, :new_connection, :in_synced
 
   def perform(database_id, new_connection = false)
-    return if cancelled?
+    super
 
-    @database = PowerbaseDatabase.find database_id
     @new_connection = new_connection
-
-    if database.in_synced?
-      puts "#{Time.now} -- SyncDatabaseWorker #{database.name} is already in synced!"
-    else
-      @unmigrated_tables = @database.unmigrated_tables
-      @deleted_tables = @database.deleted_tables
-
-      puts "#{Time.now} Migrating unmigrated tables of database with id of #{@database.id}..."
-
-      if unmigrated_tables.any?
-        batch = Sidekiq::Batch.new
-        batch.description = "#{Time.now} Migrating metadata of database with id of #{database.id}"
-        batch.on(:complete, SyncDatabaseWorker, :database_id => database.id, :step => "migrating_metadata", :new_connection => new_connection)
-        batch.jobs do
-          unmigrated_tables.each_with_index do |table_name, index|
-            table = Tables::Creator.new table_name, index + 1, database
-            table.save
-
-            # Migrate fields and records
-            table.sync!(false)
-          end
-        end
-
-        puts "#{Time.now} Started Batch #{batch.bid}"
-      end
-
-      if deleted_tables.any?
-        deleted_tables.each do |table|
-          table.remove
-        end
-      end
-    end
+    set_database(database_id)
+    initialize_sync_database
   end
 
   def on_complete(status, params)
-    puts "#{Time.now} Migrating batch for database##{params["database_id"]} has #{status.failures} failures" if status.failures != 0
+    puts "#{Time.now} -- Migrating batch for database##{params["database_id"]} has #{status.failures} failures" if status.failures != 0
 
-    @database = PowerbaseDatabase.find params["database_id"]
-    @new_connection = params["new_connection"]
+    set_database(params["database_id"])
 
-    return add_connections if params["step"] == "migrating_metadata"
-
-    if params["step"] == "adding_connections"
-      if ENV["ENABLE_LISTENER"] == "true" && database.postgresql?
-        return create_listeners
+    if params["step"] == "migrating_metadata"
+      add_connections
+    elsif params["step"] == "adding_connections" && ENV["ENABLE_LISTENER"] == "true" && database.postgresql?
+      puts "SHOULD CREATE LISTENERS NOW."
+    elsif params["step"] != "indexing_records"
+      if database.is_turbo
+        puts "INDEXING RECORDS NOW."
+      else
+        database.tables.each do |table|
+          table.write_migration_logs!(status: "migrated")
+        end
       end
     end
-
-    if database.is_turbo
-      return index_records if params["step"] != "indexing_records"
-    else
-      database.tables.each do |table|
-        table.write_migration_logs!(status: "migrated")
-      end
-    end
-
-    database.update_status!("migrated")
-    database.base_migration.end_time = Time.now
-    database.base_migration.save
-
-    listen!
-
-    pusher_trigger!("database.#{database.id}", "migration-listener", { id: database.id })
-  end
-
-  def cancelled?
-    Sidekiq.redis {|c| c.exists("cancelled-#{jid}") }
-  end
-
-  def self.cancel!(jid)
-    Sidekiq.redis {|c| c.setex("cancelled-#{jid}", 86400, 1) }
   end
 
   private
+    def set_database(database_id)
+      @database = PowerbaseDatabase.find database_id
+
+      if !@database
+        puts "Could not find database with id of '#{database_id}'"
+        return
+      end
+    end
+
+    def initialize_sync_database
+      db_syncer = Databases::Syncer.new database, new_connection: new_connection
+
+      if db_syncer.in_synced?
+        puts "#{Time.now} -- Database with id of #{database.id} is already in synced."
+        return
+      end
+
+      database.update_status!("migrating_metadata")
+
+      batch = Sidekiq::Batch.new
+      batch.description = "Migrating metadata for db##{database.id}"
+      batch.on(:complete, SyncDatabaseWorker,
+        :database_id => database.id,
+        :new_connection => new_connection,
+        :step => "migrating_metadata",
+      )
+      batch.jobs do
+        db_syncer.sync!
+      end
+
+      puts "#{Time.now} -- Started Batch #{batch.bid} - Migrating metadata for db##{database.id}"
+    end
+
+    def add_connections
+      database.update_status!("adding_connections")
+
+      batch = Sidekiq::Batch.new
+      batch.description = "Adding and auto linking connections for db##{database.id}"
+      batch.on(:complete, SyncDatabaseWorker,
+        :database_id => database.id,
+        :new_connection => new_connection,
+        :step => "adding_connections",
+      )
+      batch.jobs do
+        database.tables.each do |table|
+          table.migrator.create_base_connection_later!
+        end
+      end
+
+      puts "#{Time.now} -- Started Batch #{batch.bid} -- Adding and auto linking connections for db##{database.id}"
+    end
+
     def listen!
       listener_job = Sidekiq::Cron::Job.new(
         name: "Database #{database.id} Listener",
@@ -98,21 +99,6 @@ class SyncDatabaseWorker
         puts "#{Time.now} -- Can't run database listener cron job"
         raise listener_job.errors
       end
-    end
-
-    def add_connections
-      database.update_status!("adding_connections")
-
-      batch = Sidekiq::Batch.new
-      batch.description = "Adding and auto linking connections for database##{database.id}"
-      batch.on(:complete, SyncDatabaseWorker, :database_id => database.id, :step => "adding_connections", :new_connection => new_connection)
-      batch.jobs do
-        database.tables.each do |table|
-          table.migrator.create_base_connection_later!
-        end
-      end
-
-      puts "#{Time.now} Started Batch #{batch.bid}"
     end
 
     def create_listeners
