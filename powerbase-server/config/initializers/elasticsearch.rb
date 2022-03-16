8aa5f39b5bd5de576de0750824d70e18abeb6c9e
Joeylene Rivera <joeylenerivera@gmail.com>
2022-03-16 20:22:57 +0800

    fix: update port in elasticsearch config

--- powerbase-server/config/initializers/elasticsearch.rb
+++ powerbase-server/config/initializers/elasticsearch.rb
@@ -3,6 +3,7 @@ require 'elasticsearch/model'
 elasticsearch_url = ENV["elasticsearch_url"]
 connection_hash = {
   url: elasticsearch_url,
+  port: 443,
   scheme: "https",
   retry_on_failure: true,
   transport_options: {
