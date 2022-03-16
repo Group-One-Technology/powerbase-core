fdac69fc077d613caa29c6a72c2e58c036c494cf
Joeylene Rivera <joeylenerivera@gmail.com>
2022-03-16 19:53:55 +0800

    fix: faraday timeout

--- powerbase-server/config/initializers/elasticsearch.rb
+++ powerbase-server/config/initializers/elasticsearch.rb
@@ -1,21 +1,14 @@
 require 'elasticsearch/model'
 
-elasticsearch_url = if Rails.env.production?
-    ENV["elasticsearch_url"]
-  else
-    ENV["elasticsearch_url_development"]
-  end
-
-logger = Logger.new("#{Rails.root}/log/elasticsearch.log")
-logger.level = Logger::FATAL
-
+elasticsearch_url = ENV["elasticsearch_url"]
 connection_hash = {
   url: elasticsearch_url,
-  reload_connections: true,
-  retry_on_failure: 3,
-  request_timeout: 500,
-  logger: logger,
-  retry_on_timeout: true,
+  port: 443,
+  scheme: "https",
+  retry_on_failure: true,
+  transport_options: {
+    request: { timeout: 10 }
+  }
 }
 
 ElasticsearchClient = Elasticsearch::Client.new(connection_hash)
