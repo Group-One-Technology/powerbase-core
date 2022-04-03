8c5e956aae6b4d397ce72675342f04f53509b343
Joeylene Rivera <joeylenerivera@gmail.com>
2022-04-03 14:09:31 +0800

    refactor: update loader to span

--- powerbase-client/src/components/ui/Loader.jsx
+++ powerbase-client/src/components/ui/Loader.jsx
@@ -5,10 +5,10 @@ import { Spinner } from './Spinner';
 
 export function Loader({ className, label, ...props }) {
   return (
-    <div className={cn('w-full flex items-center justify-center', className)} {...props}>
+    <span className={cn('w-full flex items-center justify-center', className)} {...props}>
       <Spinner className="h-8 w-8 text-current" />
       {label && <span className="sr-only">{label}</span>}
-    </div>
+    </span>
   );
 }
 
