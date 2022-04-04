b6a84abddc3e9fff0df1bc6ec81e2df8515e4493
Joeylene Rivera <joeylenerivera@gmail.com>
2022-04-04 13:41:00 +0800

    feat: add drop field to ui

---
+++ powerbase-client/src/components/fields/FieldMenu/FieldMenuDrop.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import * as ContextMenu from '@radix-ui/react-context-menu';
+import { TrashIcon } from '@heroicons/react/outline';
+
+import { useSaveStatus } from '@models/SaveStatus';
+import { useViewFieldState } from '@models/view/ViewFieldState';
+import { dropField } from '@lib/api/fields';
+import { useMounted } from '@lib/hooks/useMounted';
+
+export function FieldMenuDrop({ field, setConfirmModal }) {
+  const { mounted } = useMounted();
+  const { saving, catchError, saved } = useSaveStatus();
+  const { fields, setFields, mutateViewFields } = useViewFieldState();
+
+  const handleConfirmDropField = async () => {
+    if (!field || !fields.length) return;
+
+    saving();
+    const updatedFields = fields.filter((item) => item.id !== field.id);
+    setFields(updatedFields);
+
+    try {
+      await dropField({ fieldId: field.fieldId });
+      mounted(() => {
+        mutateViewFields(updatedFields);
+        saved();
+      });
+    } catch (err) {
+      catchError(err);
+    }
+  };
+
+  const handleDropField = () => {
+    setConfirmModal({
+      open: true,
+      title: 'Drop Field',
+      description: 'Are you sure you want to drop this field? This action cannot be undone.',
+      onConfirm: handleConfirmDropField,
+    });
+  };
+
+  return (
+    <ContextMenu.Item
+      className="px-4 py-1 text-sm flex items-center cursor-pointer hover:bg-gray-100 focus:bg-gray-100"
+      onSelect={handleDropField}
+    >
+      <TrashIcon className="h-4 w-4 mr-1.5" />
+      Drop Field
+    </ContextMenu.Item>
+  );
+}
+
+FieldMenuDrop.propTypes = {
+  field: PropTypes.object.isRequired,
+  setConfirmModal: PropTypes.func.isRequired,
+};
