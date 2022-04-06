8ae90e34ebd0c7677369270fadf88d1628dac020
Joeylene Rivera <joeylenerivera@gmail.com>
2022-04-06 14:32:44 +0800

    feat: add temporary fields in create table

--- powerbase-client/src/components/tables/CreateTableModal.jsx
+++ powerbase-client/src/components/tables/CreateTableModal.jsx
@@ -1,7 +1,8 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import cn from 'classnames';
 import PropTypes from 'prop-types';
 import { XIcon } from '@heroicons/react/outline';
+import { Dialog } from '@headlessui/react';
 
 import { useBase } from '@models/Base';
 import { useFieldTypes } from '@models/FieldTypes';
@@ -12,20 +13,28 @@ import { TABLE_TYPE } from '@lib/constants/table';
 
 import { Modal } from '@components/ui/Modal';
 import { Button } from '@components/ui/Button';
+import { useViewFields } from '@models/ViewFields';
 import { InlineRadio } from '@components/ui/InlineRadio';
 import { CreateTableAlias } from './CreateTable/CreateTableAlias';
 import { CreateTableName } from './CreateTable/CreateTableName';
+import { CreateTableFields } from './CreateTable/CreateTableFields';
 
 export function CreateTableModal({ open, setOpen }) {
   const { data: base } = useBase();
   const { data: fieldTypes } = useFieldTypes();
+  const { data: viewFields } = useViewFields();
 
   const [tableName, setTableName, tableNameError] = useValidState('', SQL_IDENTIFIER_VALIDATOR);
   const [alias, setAlias, aliasError] = useValidState('', REQUIRED_VALIDATOR);
   const [tableType, setTableType] = useState(TABLE_TYPE[0]);
+  const [fields, setFields] = useState([]);
 
   const disabled = false;
   const isVirtual = tableType.nameId === 'magic_table';
+
+  useEffect(() => {
+    setFields(viewFields || []);
+  }, [viewFields]);
 
   if (!base || !fieldTypes) return null;
 
@@ -46,7 +55,9 @@ export function CreateTableModal({ open, setOpen }) {
             <XIcon className="h-4 w-4" />
           </button>
         </div>
-        <form className="mt-4 flex-1 p-4 flex flex-col" onSubmit={submit}>
+        <form className="mt-4 flex-1 py-4 px-6 flex flex-col" onSubmit={submit}>
+          <Dialog.Title className="sr-only">Create Table</Dialog.Title>
+
           <CreateTableAlias
             baseId={base.id}
             alias={alias}
@@ -59,7 +70,7 @@ export function CreateTableModal({ open, setOpen }) {
             value={tableType}
             setValue={setTableType}
             options={TABLE_TYPE}
-            className="my-6"
+            className="mt-4"
           />
 
           {!isVirtual && (
@@ -70,6 +81,11 @@ export function CreateTableModal({ open, setOpen }) {
               tableNameError={tableNameError}
             />
           )}
+
+          <CreateTableFields
+            fields={fields}
+            setFields={setFields}
+          />
 
           <div className="mt-auto mx-3">
             <Button
