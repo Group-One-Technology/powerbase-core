bc13bb1db61a119be14aa0f9847e6524606d9d07
Joeylene Rivera <joeylenerivera@gmail.com>
2022-04-12 01:51:59 +0800

    feat: add checker for virtual tables in update primary keys

--- powerbase-server/app/models/concerns/table_schema_modification.rb
+++ powerbase-server/app/models/concerns/table_schema_modification.rb
@@ -28,15 +28,16 @@ module TableSchemaModification
   end
 
   def update_primary_keys(primary_keys = [])
-    raise StandardError.new "Cannot change primary key of a migrating table." if self.status != "migrated"
+    if !self.is_virtual
+      raise StandardError.new "Cannot change primary key of a migrating table." if self.status != "migrated"
 
-    constraints = self.db.connections
-      .filter {|connection| connection.referenced_table_id == self.id && connection.is_constraint}
+      constraints = self.db.connections
+        .filter {|connection| connection.referenced_table_id == self.id && connection.is_constraint}
 
-    if constraints.length > 0
-      raise StandardError.new "Cannot update primary key because it is needed in a foreign key constraint. Remove referenced foreign key constraint first to update this table's primary key."
+      if constraints.length > 0
+        raise StandardError.new "Cannot update primary key because it is needed in a foreign key constraint. Remove referenced foreign key constraint first to update this table's primary key."
+      end
     end
-
 
     fields = self.fields
     turbo = self.db.is_turbo
@@ -44,13 +45,15 @@ module TableSchemaModification
     primary_key_fields = fields.select {|field| primary_keys.include?(field.name)}
     virtual_fields = fields.select {|item| item.is_virtual}
 
-    if !turbo && virtual_fields.any? && primary_key_fields.length == 0
-      raise StandardError.new "There must be at least one primary key fields because it is needed for the magic fields."
-    end
+    if !self.is_virtual
+      if !turbo && virtual_fields.any? && primary_key_fields.length == 0
+        raise StandardError.new "There must be at least one primary key fields because it is needed for the magic fields."
+      end
 
-    primary_key_virtual_fields = primary_key_fields.select {|field| field.is_virtual }
-    if primary_key_virtual_fields.any?
-      raise StandardError.new "Cannot set a magic field as a primary key."
+      primary_key_virtual_fields = primary_key_fields.select {|field| field.is_virtual }
+      if primary_key_virtual_fields.any?
+        raise StandardError.new "Cannot set a magic field as a primary key."
+      end
     end
 
     primary_key_pii_fields = primary_key_fields.select {|field| field.is_pii }
@@ -66,50 +69,52 @@ module TableSchemaModification
 
     return if primary_keys.sort == current_primary_keys.sort
 
-    is_postgresql = self.db.postgresql?
-    indexes = if is_postgresql
-        _sequel.fetch("
-          SELECT ix.relname as index_name,
-            upper(am.amname) AS index_algorithm,
-            indisunique as is_unique,
-            pg_get_indexdef(indexrelid) as index_definition
-          FROM pg_index i
-          JOIN pg_class t ON t.oid = i.indrelid
-          JOIN pg_class ix ON ix.oid = i.indexrelid
-          JOIN pg_namespace n ON t.relnamespace = n.oid
-          JOIN pg_am as am ON ix.relam = am.oid
-          WHERE t.relname = '#{self.name}' AND n.nspname = 'public';
-        ").all
-      else
-        _sequel.fetch("
-          SELECT index_name as index_name,
-            index_type AS index_algorithm,
-            CASE non_unique
-              WHEN 0 THEN 'TRUE'
-              ELSE 'FALSE'
-            END AS is_unique,
-            column_name as column_name
-          FROM information_schema.statistics
-          WHERE table_schema='#{self.db.database_name}' AND table_name='#{self.name}' ORDER BY seq_in_index ASC;
-        ").all
+    if !self.is_virtual
+      is_postgresql = self.db.postgresql?
+      indexes = if is_postgresql
+          _sequel.fetch("
+            SELECT ix.relname as index_name,
+              upper(am.amname) AS index_algorithm,
+              indisunique as is_unique,
+              pg_get_indexdef(indexrelid) as index_definition
+            FROM pg_index i
+            JOIN pg_class t ON t.oid = i.indrelid
+            JOIN pg_class ix ON ix.oid = i.indexrelid
+            JOIN pg_namespace n ON t.relnamespace = n.oid
+            JOIN pg_am as am ON ix.relam = am.oid
+            WHERE t.relname = '#{self.name}' AND n.nspname = 'public';
+          ").all
+        else
+          _sequel.fetch("
+            SELECT index_name as index_name,
+              index_type AS index_algorithm,
+              CASE non_unique
+                WHEN 0 THEN 'TRUE'
+                ELSE 'FALSE'
+              END AS is_unique,
+              column_name as column_name
+            FROM information_schema.statistics
+            WHERE table_schema='#{self.db.database_name}' AND table_name='#{self.name}' ORDER BY seq_in_index ASC;
+          ").all
+        end
+
+      key_query = is_postgresql ? "pk" : "primary"
+      primary_key_index = indexes.select {|index| index[:index_name].downcase.include?(key_query) && index[:is_unique]}.first
+
+      if primary_key_index
+        _sequel.alter_table(table_name) do
+          drop_constraint(primary_key_index[:index_name], type: :primary_key)
+        end
       end
 
-    key_query = is_postgresql ? "pk" : "primary"
-    primary_key_index = indexes.select {|index| index[:index_name].downcase.include?(key_query) && index[:is_unique]}.first
-
-    if primary_key_index
-      _sequel.alter_table(table_name) do
-        drop_constraint(primary_key_index[:index_name], type: :primary_key)
+      if primary_keys.length > 0
+        _sequel.alter_table(table_name) do
+          add_primary_key(primary_keys)
+        end
       end
+
+      self._sequel.disconnect
     end
-
-    if primary_keys.length > 0
-      _sequel.alter_table(table_name) do
-        add_primary_key(primary_keys)
-      end
-    end
-
-    self._sequel.disconnect
 
     old_primary_keys = fields
       .select {|field| field.is_primary_key }
