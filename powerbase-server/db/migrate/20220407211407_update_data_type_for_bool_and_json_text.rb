e37ad79c67589f6171401d35f1335c776e3f7584
Joeylene Rivera <joeylenerivera@gmail.com>
2022-05-02 10:18:26 +0800

    fix: update migration

--- powerbase-server/db/migrate/20220407211407_update_data_type_for_bool_and_json_text.rb
+++ powerbase-server/db/migrate/20220407211407_update_data_type_for_bool_and_json_text.rb
@@ -1,9 +1,9 @@
 class UpdateDataTypeForBoolAndJsonText < ActiveRecord::Migration[6.1]
   def change
     json_text = PowerbaseFieldType.find_by(name: "JSON Text")
-    json_text.update(data_type: "string")
+    json_text.update(data_type: "string") if json_text
 
     bool = FieldDbTypeMapping.find_by(db_type: "boolean", adapter: "sequel")
-    bool.update(db_type: "bool")
+    bool.update(db_type: "bool") if bool
   end
 end
