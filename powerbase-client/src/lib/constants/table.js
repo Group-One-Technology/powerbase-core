3a4fa27a309eda7d9b043dfc55ba9cffa40252e6
Joeylene Rivera <joeylenerivera@gmail.com>
2022-05-03 11:45:09 +0800

    refactor: updater table type order

--- powerbase-client/src/lib/constants/table.js
+++ powerbase-client/src/lib/constants/table.js
@@ -1,14 +1,14 @@
 export const TABLE_TYPE = [
   {
-    nameId: 'magic_table',
-    descriptionId: 'magic_table_description',
-    name: 'Magic Table',
-    description: 'Accessible thru powerbase but will not affect your current database.',
-  },
-  {
     nameId: 'persistent_table',
     descriptionId: 'persistent_table_description',
     name: 'Persistent Table',
     description: 'Creates a new table in your SQL table.',
+  },
+  {
+    nameId: 'magic_table',
+    descriptionId: 'magic_table_description',
+    name: 'Magic Table',
+    description: 'Accessible thru powerbase but will not affect your current database.',
   },
 ];
