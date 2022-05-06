81c6d0699b1811aa106db5a5ba57e645c69c075a
Joeylene Rivera <joeylenerivera@gmail.com>
2022-05-06 15:29:51 +0800

    feat: add self hosted web socker - poxa

--- powerbase-client/src/lib/hooks/websockets/index.jsx
+++ powerbase-client/src/lib/hooks/websockets/index.jsx
@@ -1,6 +1,21 @@
 import Pusher from 'pusher-js';
 
-const { PUSHER_KEY } = process.env;
-export const pusher = new Pusher(PUSHER_KEY, {
-  cluster: 'ap1',
-});
+const { PUSHER_KEY, PUSHER_HOST } = process.env;
+let host = '';
+let port = '';
+
+if (PUSHER_HOST.length > 0) {
+  [, host] = PUSHER_HOST.split('://');
+  [host, port] = host.split(':');
+}
+
+const config = host.length > 0 && port.length > 0
+  ? {
+    wsHost: host,
+    wsPort: Number(port),
+    forceTLS: false,
+    disableStats: true,
+    enabledTransports: ['ws', 'wss'],
+  } : { cluster: 'ap1' };
+
+export const pusher = new Pusher(PUSHER_KEY, config);
