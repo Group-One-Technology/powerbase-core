731606cd5de9ffebfb03a1119ce0ff3621ba9800
Joeylene Rivera <joeylenerivera@gmail.com>
2022-06-10 18:28:10 +0800

    fix: update formatting for number

--- powerbase-client/src/lib/helpers/fields/sanitizeFieldValue.js
+++ powerbase-client/src/lib/helpers/fields/sanitizeFieldValue.js
@@ -4,10 +4,15 @@ export function sanitizeValue({ field, fieldType, value }) {
   switch (fieldType?.name) {
     case FieldType.CHECKBOX: return value?.toString() === 'true';
     case FieldType.NUMBER: {
+      const number = typeof value !== 'number'
+        ? parseFloat(value)
+        : value;
+
       if (field.options?.precision) {
-        return parseFloat(value).toFixed(field.options.precision);
+        return number.toFixed(field.options.precision);
       }
-      return parseFloat(value);
+
+      return number;
     }
     default: {
       return value;
