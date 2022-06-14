c976b72223a3ce8a908b8d5445ae0b9963a8545b
Joeylene Rivera <joeylenerivera@gmail.com>
2022-06-14 09:11:31 +0800

    refactor: remove faker and mock data

--- powerbase-client/src/lib/mock/userMock.js
+++
@@ -1,8 +0,0 @@
-export const user = {
-  id: '5c7bf450-6d9e-4be3-9603-9876ce9e809f',
-  firstName: 'Darby',
-  lastName: 'Von',
-  name: 'Darby Von',
-  email: 'darbyvon@gmail.com',
-  displayPhotoUrl: 'https://i.pravatar.cc/150?img=67',
-};
