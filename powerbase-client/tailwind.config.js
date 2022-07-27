6895422152b7254b4244fb7751530bf0834489de
Joeylene Rivera <joeylenerivera@gmail.com>
2022-07-28 02:19:42 +0800

    fix: font size overriden

--- powerbase-client/tailwind.config.js
+++ powerbase-client/tailwind.config.js
@@ -6,7 +6,17 @@ module.exports = {
   content: ['./public/*.html', './src/**/*.{js,jsx}'],
   theme: {
     fontSize: {
-      sm: '.8rem',
+      xs: '.75rem',
+      sm: '0.8rem',
+      base: '1rem',
+      lg: '1.125rem',
+      xl: '1.25rem',
+      '2xl': '1.5rem',
+      '3xl': '1.875rem',
+      '4xl': '2.25rem',
+      '5xl': '3rem',
+      '6xl': '4rem',
+      '7xl': '5rem',
     },
     extend: {
       fontFamily: {
